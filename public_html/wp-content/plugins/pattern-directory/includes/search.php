81ede01743c629daa5f406f1aab6b82faeafa031
Ian Dunn <ian@iandunn.name>
2021-02-18 17:10:48 -0800

    Search: Integrate ElasticSearch.

--- public_html/wp-content/plugins/pattern-directory/includes/search.php
+++ public_html/wp-content/plugins/pattern-directory/includes/search.php
@@ -2,8 +2,16 @@
 
 namespace WordPressdotorg\Pattern_Directory\Search;
 
+use WP_Query, Jetpack_WPES_Search_Query_Parser;
+use const WordPressdotorg\Pattern_Directory\Pattern_Post_Type\POST_TYPE;
 
+// Note: This depends on the Search module and custom index being enabled in `mu-plugins/main-network/site-patterns.php`.
+
+add_filter( 'jetpack_search_should_handle_query', __NAMESPACE__ . '\should_handle_query', 10, 2 );
+add_filter( 'jetpack_search_es_query_args', __NAMESPACE__ . '\modify_es_query_args', 10, 2 );
 add_filter( 'jetpack_sync_post_meta_whitelist', __NAMESPACE__ . '\sync_pattern_meta' );
+add_action( 'jetpack_search_abort', __NAMESPACE__ . '\log_aborted_queries', 10, 2 );
+add_action( 'failed_jetpack_search_query', __NAMESPACE__ . '\log_failed_queries' );
 
 
 /**
@@ -11,7 +19,7 @@ add_filter( 'jetpack_sync_post_meta_whitelist', __NAMESPACE__ . '\sync_pattern_m
  *
  * @param array $post_meta_safelist
  *
- * @return mixed
+ * @return array
  */
 function sync_pattern_meta( $post_meta_safelist ) {
 	$post_meta_safelist[] = 'wpop_description';
@@ -19,4 +27,131 @@ function sync_pattern_meta( $post_meta_safelist ) {
 	$post_meta_safelist[] = 'wpop_locale';
 
 	return $post_meta_safelist;
+}
+
+/**
+ * Determine if the search query should be handled by Jetpack/ElasticSearch.
+ *
+ * Our ES query modifications should only affect the REST API (which includes the front end, since the theme makes
+ * XHR requests to fetch patterns). We don't want to restrict wp-admin list tables, since that could make it
+ * difficult to find variations, etc.
+ *
+ * @param bool     $handle_query
+ * @param WP_Query $query
+ *
+ * @return bool
+ */
+function should_handle_query( $handle_query, $query ) {
+	/*
+	 * This isn't really what `wp_is_json_request()` is meant for, but it's the best option until something like
+	 * `wp_doing_rest()` is available.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/42061
+	 */
+	return wp_is_json_request() && $query->is_search() && POST_TYPE === $query->get( 'post_type' );
+}
+
+/**
+ * Customize the ES query for patterns.
+ *
+ * @see `should_handle_query` has preconditions for this function.
+ *
+ * @param array    $es_query_args The raw Elasticsearch query args.
+ * @param WP_Query $wp_query      The original WP_Query object.
+ *
+ * @return array
+ */
+function modify_es_query_args( $es_query_args, $wp_query ) {
+	$user_query = $wp_query->get( 's' );
+	$locale     = $wp_query->get( 'meta_query' )['orderby_locale']['value']['0'];
+
+	jetpack_require_lib( 'jetpack-wpes-query-builder/jetpack-wpes-query-parser' );
+
+	$parser = new Jetpack_WPES_Search_Query_Parser( $wp_query, array() );
+
+	$must_query = [
+		'multi_match' => [
+			'query'    => $user_query,
+			'fields'   => [ 'title_en', 'meta.wpop_description.value' ],
+			'boost'    => 0.1,
+			'operator' => 'and',
+		],
+	];
+
+	$should_query = [
+		[
+			'multi_match' => [
+				'query'    => $user_query,
+				'fields'   => [ 'title_en' ],
+				'boost'    => 2,
+				'operator' => 'and',
+			],
+		],
+
+		[
+			'multi_match' => [
+				/*
+				 * The `description_en` field in the ES index is actually `post_content`, but that's not
+				 * relevant in this context, since that's just sample contend. The `wpop_description`
+				 * field is the actual description that should be searched.
+				 */
+				'fields'   => [ 'meta.wpop_description.value' ],
+				'query'    => $user_query,
+				'boost'    => 1,
+				// todo not working. making this higher than title should boost p15 above p19 but it doesn't
+				'operator' => 'and',
+			],
+		],
+	];
+
+	$filter = [
+		'bool' => [
+			'must' => [
+				[ 'term' => [ 'post_type' => 'wporg-pattern' ] ],
+				[ 'term' => [ 'meta.wpop_locale.value.raw' => $locale ] ],
+				[ 'term' => [ 'taxonomy.wporg-pattern-keyword.slug' => 'core' ] ],
+			]
+		],
+	];
+
+	$parser->add_query( $must_query, 'must' );
+	$parser->add_query( $should_query, 'should' );
+	$parser->add_filter( $filter );
+
+	$es_query_args['query']  = $parser->build_query();
+	$es_query_args['filter'] = $parser->build_filter();
+
+	return $es_query_args;
+}
+
+/**
+ * Log when Jetpack does not run the query.
+ *
+ * @param string $reason
+ * @param array  $data
+ */
+function log_aborted_queries( $reason, $data ) {
+	if ( defined( 'WPORG_SANDBOXED' ) && WPORG_SANDBOXED ) {
+		wp_send_json_error( array( 'jetpack_search_abort - ' . $reason, $data ) );
+
+	} else {
+			error_log( 'jetpack_search_abort - cc @iandunn, @tellyworth, @dd32 - ' . $reason . ' - ' . wp_json_encode( $data ), E_USER_ERROR );
+	}
+}
+
+/**
+ * Log when Jetpack gets an error running the query.
+ *
+ * This filter doesn't currently work, but should in the future.
+ * @link https://github.com/Automattic/jetpack/issues/18888
+ *
+ * @param array $data
+ */
+function log_failed_queries( $data ) {
+	if ( defined( 'WPORG_SANDBOXED' ) && WPORG_SANDBOXED ) {
+		wp_send_json_error( array( 'failed_jetpack_search_query', $data ) );
+
+	} else {
+			error_log( 'failed_jetpack_search_query - cc @iandunn, @tellyworth, @dd32 - ' . wp_json_encode( $data ), E_USER_ERROR );
+	}
 }
