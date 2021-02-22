553d9d33c23e9ff91a80e6131c5c8445a152aee4
Ian Dunn <ian@iandunn.name>
2021-02-22 10:21:19 -0800

    Search: Add pattern meta to ElasticSearch index
    See #28

--- public_html/wp-content/plugins/pattern-directory/bootstrap.php
+++ public_html/wp-content/plugins/pattern-directory/bootstrap.php
@@ -10,3 +10,4 @@ namespace WordPressdotorg\Pattern_Directory;
 
 require_once __DIR__ . '/includes/pattern-post-type.php';
 require_once __DIR__ . '/includes/pattern-validation.php';
+require_once __DIR__ . '/includes/search.php';
---
+++ public_html/wp-content/plugins/pattern-directory/includes/search.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WordPressdotorg\Pattern_Directory\Search;
+
+
+add_filter( 'jetpack_sync_post_meta_whitelist', __NAMESPACE__ . '\sync_pattern_meta' );
+
+
+/**
+ * Tell Jetpack to sync pattern meta, so it can be indexed by ElasticSearch.
+ *
+ * @param $post_meta_safelist
+ *
+ * @return mixed
+ */
+function sync_pattern_meta( $post_meta_safelist ) {
+	$post_meta_safelist[] = 'wpop_description';
+	$post_meta_safelist[] = 'wpop_viewport_width';
+
+	return $post_meta_safelist;
+}
