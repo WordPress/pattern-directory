511a74e948920843448a244f82c0503f3754016d
dufresnesteven <steve.dufresne@automattic.com>
2021-04-01 12:24:16 +0900

    Update mobile view to have side label.

--- public_html/wp-content/themes/pattern-directory/src/components/category-menu/mobile.js
+++ public_html/wp-content/themes/pattern-directory/src/components/category-menu/mobile.js
@@ -1,11 +1,18 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { SelectControl } from '@wordpress/components';
 import { ifViewportMatches } from '@wordpress/viewport';
 
-const MobileMenu = ( { onClick, options } ) => (
-	<SelectControl label="Categories" options={ options } onChange={ onClick } />
+const MobileMenu = ( { onClick, path, options } ) => (
+	<SelectControl
+		labelPosition="side"
+		label={ __( 'Categories', 'wporg-patterns' ) }
+		value={ path }
+		options={ options }
+		onChange={ onClick }
+	/>
 );
 
 export default ifViewportMatches( '< medium' )( MobileMenu );
