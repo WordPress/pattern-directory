336e11044392d648def374e12d9f978f76c37258
dufresnesteven <steve.dufresne@automattic.com>
2021-04-01 10:11:50 +0900

    Add a category component.

--- public_html/wp-content/themes/pattern-directory/src/components/category-search/index.js
+++ public_html/wp-content/themes/pattern-directory/src/components/category-search/index.js
@@ -1,5 +1,22 @@
-const CategorySearch = ( props ) => {
-	return <input />;
+/**
+ * External dependencies
+ */
+import { __ } from '@wprd[ress/i18n';
+import { Icon, search } from '@wordpress/icons';
+
+const CategorySearch = () => {
+	return (
+		<form method="get" action="/" className="category-search">
+			<label for="pattern-search" className="screen-reader-text">
+				{ __( 'Search for:', 'wporg-patterns' ) }
+			</label>
+			<input id="pattern-search" type="search" placeholder={ __( 'Search patterns', 'wporg-patterns' ) } />
+			<button className="category-search__button">
+				<span className="screen-reader-text"> { __( 'Search patterns', 'wporg-patterns' ) }</span>
+				<Icon icon={ search } />
+			</button>
+		</form>
+	);
 };
 
 export default CategorySearch;
