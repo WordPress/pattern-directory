1930bdad0ebe5db2f86702ced4671b51fc35410e
Kelly Dwan <ryelle@users.noreply.github.com>
2021-04-02 18:14:36 -0400

    Add live pattern previews to the PatternGrid

--- public_html/wp-content/themes/pattern-directory/src/components/pattern-thumbnail/index.js
+++ public_html/wp-content/themes/pattern-directory/src/components/pattern-thumbnail/index.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Button, Tooltip } from '@wordpress/components';
+import { Button, Disabled, Tooltip } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 
 /**
@@ -10,6 +10,7 @@ import { useState } from '@wordpress/element';
  */
 import IconHeartOutline from '../icons/heart-outline';
 import IconHeartFilled from '../icons/heart-filled';
+import Canvas from './canvas';
 
 function PatternThumbnail( { pattern } ) {
 	// @todo Implement a real favoriting process.
@@ -19,6 +20,9 @@ function PatternThumbnail( { pattern } ) {
 		<div className="pattern-grid__pattern">
 			<a href={ pattern.link } rel="bookmark">
 				<span className="screen-reader-text">{ pattern.title.rendered }</span>
+				<Disabled>
+					<Canvas className="pattern-grid__preview" html={ pattern.content.rendered } />
+				</Disabled>
 			</a>
 			<div className="pattern-grid__actions">
 				<h2 className="pattern-grid__title">{ pattern.title.raw }</h2>
