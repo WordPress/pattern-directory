d135a15a474091ff630e534deac6ce0477b9a82d
dufresnesteven <steve.dufresne@automattic.com>
2021-04-08 15:14:44 +0900

    Fix php linter issues.

--- public_html/wp-content/plugins/pattern-directory/includes/pattern-taxonomy-modifications.php
+++ public_html/wp-content/plugins/pattern-directory/includes/pattern-taxonomy-modifications.php
@@ -2,16 +2,16 @@
 
 namespace WordPressdotorg\Pattern_Directory\Pattern_Taxonomy_Modifications;
 
-$CATEGORY_TAXONOMY = 'wporg-pattern-category';
+$category_taxonomy = 'wporg-pattern-category';
 const CATEGORY_TAXONOMY_ID = 'display-order';
 
-add_action( "{$CATEGORY_TAXONOMY}_add_form_fields", __NAMESPACE__ . '\add_ordering_form_field', 0 );
-add_action( "{$CATEGORY_TAXONOMY}_edit_form_fields", __NAMESPACE__ . '\edit_ordering_form_field', 10, 2 );
-add_action( "created_{$CATEGORY_TAXONOMY}", __NAMESPACE__ . '\save_term_field' );
-add_action( "edited_{$CATEGORY_TAXONOMY}", __NAMESPACE__ . '\save_term_field' );
-add_filter( "manage_{$CATEGORY_TAXONOMY}_custom_column", __NAMESPACE__ . '\add_category_order_column_content', 10, 3 );
-add_filter( "manage_edit-{$CATEGORY_TAXONOMY}_columns", __NAMESPACE__ . '\add_order_table_heading' );
-add_filter( "rest_prepare_{$CATEGORY_TAXONOMY}", __NAMESPACE__ . '\rest_prepare_category_response', 10, 2 );
+add_action( "{$category_taxonomy}_add_form_fields", __NAMESPACE__ . '\add_ordering_form_field', 0 );
+add_action( "{$category_taxonomy}_edit_form_fields", __NAMESPACE__ . '\edit_ordering_form_field', 10, 2 );
+add_action( "created_{$category_taxonomy}", __NAMESPACE__ . '\save_term_field' );
+add_action( "edited_{$category_taxonomy}", __NAMESPACE__ . '\save_term_field' );
+add_filter( "manage_{$category_taxonomy}_custom_column", __NAMESPACE__ . '\add_category_order_column_content', 10, 3 );
+add_filter( "manage_edit-{$category_taxonomy}_columns", __NAMESPACE__ . '\add_order_table_heading' );
+add_filter( "rest_prepare_{$category_taxonomy}", __NAMESPACE__ . '\rest_prepare_category_response', 10, 2 );
 
 /**
  * Adds a form field for adding an ordinal number to a new category.
@@ -33,7 +33,7 @@ function add_ordering_form_field() {
  *
  * This is a callback for the `{$taxonomy}_edit_form_fields` filter, and it's used to modify the edit taxonomy form.
  *
- * @param WP_Term $term Term object
+ * @param WP_Term $term Term object.
  */
 function edit_ordering_form_field( $term ) {
 	$value = get_term_meta( $term->term_id, CATEGORY_TAXONOMY_ID, true );
@@ -57,7 +57,7 @@ function edit_ordering_form_field( $term ) {
  *
  * This is a callback for the `created_{$taxonomy}` and `edited_{$taxonomy}` filter, and it's used to save meta information to the database.
  *
- * @param integer $term_id Term id
+ * @param integer $term_id Term id.
  */
 function save_term_field( $term_id ) {
 	update_term_meta(
@@ -78,7 +78,7 @@ function save_term_field( $term_id ) {
  */
 function add_order_table_heading( $columns ) {
 	// Our our custom column heading
-	$columns[ CATEGORY_TAXONOMY_ID ] = __( 'Display Order' );
+	$columns[ CATEGORY_TAXONOMY_ID ] = __( 'Display Order', 'wporg-patterns' );
 
 	return $columns;
 }
@@ -111,14 +111,14 @@ function add_category_order_column_content( $content, $column, $term_id ) {
  * @return WP_REST_Response
  */
 function rest_prepare_category_response( $response, $term ) {
-	$orderNum = get_term_meta( $term->term_id, CATEGORY_TAXONOMY_ID, true );
+	$order_num = get_term_meta( $term->term_id, CATEGORY_TAXONOMY_ID, true );
 
 	// If it's empty we'll make it very low priority
-	if ( empty( $orderNum ) ) {
-		$orderNum = 9999;
+	if ( empty( $order_num ) ) {
+		$order_num = 9999;
 	}
 
-	$response->data['display_order'] = (int) $orderNum;
+	$response->data['display_order'] = (int) $order_num;
 
 	return $response;
 }
