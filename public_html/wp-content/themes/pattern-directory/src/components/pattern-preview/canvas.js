a1093822ab72572be6cdb87b28f9462f208286e9
Kelly Dwan <ryelle@users.noreply.github.com>
2021-04-27 10:04:17 -0400

    Pattern Previews: Always add a theme stylesheet to preview iframes (#87)
    * Pattern Previews: Always add a theme stylesheet to preview iframes
    
    * Swap out unnecessary backticks

--- public_html/wp-content/themes/pattern-directory/src/components/pattern-preview/canvas.js
+++ public_html/wp-content/themes/pattern-directory/src/components/pattern-preview/canvas.js
@@ -1,10 +1,7 @@
 /**
- * WordPress dependencies
+ * Internal dependencies
  */
-import {
-	/* eslint-disable-next-line @wordpress/no-unsafe-wp-apis -- Experimental is OK. */
-	__unstableIframe as Iframe,
-} from '@wordpress/block-editor';
+import Iframe from '../iframe';
 
 function Canvas( { html } ) {
 	const style = {
@@ -13,17 +10,13 @@ function Canvas( { html } ) {
 		minHeight: '600px',
 		overflowY: 'auto',
 	};
-	const extraIframeStyles =
-		// @todo - Should we keep the TT1 style? Load css from a local file?
-		'<link rel="stylesheet" id="twenty-twenty-one-style-css"  href="https://wp-themes.com/wp-content/themes/twentytwentyone/style.css?ver=1.2" media="all" />' +
-		'<style>body{pointer-events:none;display: flex;align-items: center;justify-content: center;min-height: 100vh;} body > div {width: 100%}</style>';
 
 	return (
 		<div>
 			<Iframe
 				className="pattern-preview__viewport-iframe"
 				style={ style }
-				headHTML={ window.__editorStyles.html + extraIframeStyles }
+				headHTML={ window.__editorStyles.html }
 			>
 				<div
 					dangerouslySetInnerHTML={ {
