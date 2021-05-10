9d1207f41e7fe7fb606e20d9a573443b0c9ffb69
dufresnesteven <steve.dufresne@automattic.com>
2021-05-10 11:48:53 +0900

    Add in the real svg's even though backgrounds are not working properly.

--- public_html/wp-content/plugins/pattern-creator/src/components/welcome-guide/index.js
+++ public_html/wp-content/plugins/pattern-creator/src/components/welcome-guide/index.js
@@ -2,14 +2,14 @@
  * WordPress dependencies
  */
 import { ExternalLink, Guide } from '@wordpress/components';
-import { useSelect, useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { store as editPostStore } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { CanvasImage, EditorImage, BlockLibraryImage, DocumentationImage, InserterIconImage } from './images';
+import { ImageCollectionImage, PatternDesignImage, PatternEditorImage, PatternsImage } from './images';
 
 /**
  * Module constants
@@ -28,69 +28,88 @@ export default function WelcomeGuide() {
 	return (
 		<Guide
 			className="pattern-creator-welcome-guide"
-			contentLabel={ __( 'Welcome to the pattern creator' ) }
-			finishButtonText={ __( 'Get started' ) }
+			contentLabel={ __( 'Welcome to the pattern creator', 'wporg-patterns' ) }
+			finishButtonText={ __( 'Get started', 'wporg-patterns' ) }
 			onFinish={ () => toggleFeature( GUIDE_ID ) }
 			pages={ [
 				{
-					image: <CanvasImage />,
+					image: (
+						<div className="pattern-creator-welcome-guide__image circle-background">
+							<PatternsImage />
+						</div>
+					),
 					content: (
 						<>
 							<h1 className="pattern-creator-welcome-guide__title">
-								{ __( 'Welcome to the pattern editor' ) }
+								{ __( 'Welcome to the pattern editor', 'wporg-patterns' ) }
 							</h1>
 							<p>
 								{ __(
-									'Mix and match WordPress blocks together to create unique and compelling designs.'
+									'Mix and match WordPress blocks together to create unique and compelling designs.',
+									'wporg-patterns'
 								) }
 							</p>
 						</>
 					),
 				},
 				{
-					image: <EditorImage />,
+					image: (
+						<div className="pattern-creator-welcome-guide__image diamond-background">
+							<ImageCollectionImage />
+						</div>
+					),
 					content: (
 						<>
 							<h1 className="pattern-creator-welcome-guide__title">
-								{ __( 'Use our collection of license-free images' ) }
+								{ __( 'Use our collection of license-free images', 'wporg-patterns' ) }
 							</h1>
 							<p>
 								{ __(
-									'Don’t worry about licensing. We’ve provided a collection of worry-free images and media for you to use.'
+									'Don’t worry about licensing. We’ve provided a collection of worry-free images and media for you to use.',
+									'wporg-patterns'
 								) }
 							</p>
 						</>
 					),
 				},
 				{
-					image: <EditorImage />,
+					image: (
+						<div className="pattern-creator-welcome-guide__image pattern-creator-welcome-guide__image--is-bottom-aligned triangles-background">
+							<PatternEditorImage />
+						</div>
+					),
 					content: (
 						<>
 							<h1 className="pattern-creator-welcome-guide__title">
-								{ __( 'Find your patterns in the editor' ) }
+								{ __( 'Find your patterns in the editor', 'wporg-patterns' ) }
 							</h1>
 							<p>
 								{ __(
-									'Patterns in the directory are availabe from any WordPress editor. Patterns you’ve created and favorited will be highlight for easy access.'
+									'Patterns in the directory are available from any WordPress editor. Patterns you’ve created and favorited will be highlight for easy access.',
+									'wporg-patterns'
 								) }
 							</p>
 						</>
 					),
 				},
 				{
-					image: <EditorImage />,
+					image: (
+						<div className="pattern-creator-welcome-guide__image pattern-creator-welcome-guide__image--is-bottom-aligned square-background">
+							<PatternDesignImage />
+						</div>
+					),
 					content: (
 						<>
 							<h1 className="pattern-creator-welcome-guide__title">
-								{ __( 'Learn more about designing patterns' ) }
+								{ __( 'Learn more about designing patterns', 'wporg-patterns' ) }
 							</h1>
 							<p>
-								{ __( 'New to patterns? Want to learn more about building them?' ) }
+								{ __( 'New to patterns? Want to learn more about building them?', 'wporg-patterns' ) }
 								<ExternalLink
 									className="pattern-creator-welcome-guide__link"
-									href={ __( 'https://wordpress.org/support/article/wordpress-editor/' ) }
+									href={ __( 'https://wordpress.org/support/article/wordpress-editor/', 'wporg-patterns' ) }
 								>
-									{ __( "Here's a detailed guide." ) }
+									{ __( "Here's a detailed guide.", 'wporg-patterns' ) }
 								</ExternalLink>
 							</p>
 						</>
