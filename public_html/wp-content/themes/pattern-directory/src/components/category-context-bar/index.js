a8b9c89330eaf76b031194f38c9ce13e64fa9008
Steven Dufresne <steve.dufresne@automattic.com>
2021-05-20 09:29:37 +0900

    Theme: Update `CategoryContextBar ` with new loading UI. (#106)

--- public_html/wp-content/themes/pattern-directory/src/components/category-context-bar/index.js
+++ public_html/wp-content/themes/pattern-directory/src/components/category-context-bar/index.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { Spinner } from '@wordpress/components';
 import { useEffect, useRef, useState } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { getQueryArg } from '@wordpress/url';
@@ -10,13 +11,17 @@ import { getQueryArg } from '@wordpress/url';
  */
 import { useRoute } from '../../hooks';
 import { getCategoryFromPath } from '../../utils';
-import { getContextMessage } from './messaging';
+import { getAllSearchMessage, getDefaultMessage, getDefaultSearchMessage, getLoadingMessage } from './messaging';
 import { store as patternStore } from '../../store';
 
 function CategoryContextBar() {
 	const { path } = useRoute();
 	const [ height, setHeight ] = useState( 0 );
-	const [ context, setContext ] = useState( {} );
+	const [ message, setMessage ] = useState();
+	const [ context ] = useState( {
+		title: '',
+		links: [],
+	} );
 	const innerRef = useRef( null );
 
 	const { isAllCategory, category, isLoadingPatterns, patterns } = useSelect(
@@ -39,42 +44,49 @@ function CategoryContextBar() {
 	);
 
 	useEffect( () => {
-		if ( ! category || isLoadingPatterns ) {
+		if ( ! category ) {
+			return;
+		}
+
+		if ( isLoadingPatterns ) {
+			setMessage( getLoadingMessage( category.name ) );
 			return;
 		}
 
 		const searchTerm = getQueryArg( path, 'search' );
 
-		let _context = {};
-
-		// Use the category count as default since it has the count of all the associated patterns
-		let count = category.count;
-
-		// If we have a search term use the pattern results length.
-		// Not: This is okay until we start using paging.
-		if ( searchTerm ) {
-			count = patterns.length;
+		if ( searchTerm && ! isAllCategory ) {
+			setMessage( getDefaultSearchMessage( patterns.length, category.name, searchTerm ) );
+		} else if ( searchTerm && isAllCategory ) {
+			setMessage( getAllSearchMessage( patterns.length, searchTerm ) );
+		} else if ( ! isAllCategory ) {
+			setMessage( getDefaultMessage( category.count || 0, category.name ) );
+		} else {
+			setMessage( '' );
 		}
-
-		if ( ! isAllCategory || searchTerm ) {
-			_context = getContextMessage( count, category.name, searchTerm );
-		}
-
-		setContext( _context );
 	}, [ category, isLoadingPatterns, patterns ] );
 
 	useEffect( () => {
-		const _height = context.message ? innerRef.current.offsetHeight : 0;
+		const _height = message ? innerRef.current.offsetHeight : 0;
 		setHeight( _height );
-	}, [ context.message ] );
+	}, [ message ] );
 
 	return (
-		<header className="category-context__bar" style={ { height: `${ height }px` } }>
+		<header className="category-context-bar" style={ { height: `${ height }px` } }>
 			<div ref={ innerRef }>
-				<h2 className="category-context__bar__copy">{ context.message }</h2>
+				<h2 className="category-context-bar__copy">
+					<span
+						className={ `category-context-bar__spinner ${
+							! isLoadingPatterns ? 'category-context-bar__spinner--is-hidden' : ''
+						}` }
+					>
+						<Spinner />
+					</span>
+					<span>{ message }</span>
+				</h2>
 				{ context.links && context.links.length > 0 && (
-					<div className="category-context__bar__links">
-						<h3 className="category-context__bar__title">{ context.title }</h3>
+					<div className="category-context-bar__links">
+						<h3 className="category-context-bar__title">{ context.title }</h3>
 
 						<ul>
 							{ context.links.map( ( i ) => (
