5725910450279b3ab75356b264b8c35ef3e93402
dufresnesteven <steve.dufresne@automattic.com>
2021-05-21 14:42:06 +0900

    Add in a confirmation modal.

---
+++ public_html/wp-content/plugins/pattern-creator/src/components/publish-modal/submit.js
@@ -0,0 +1,161 @@
+/**
+ * WordPress dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+import { Button, CheckboxControl, Modal, TextControl, TextareaControl } from '@wordpress/components';
+import { store as editorStore } from '@wordpress/editor';
+import { useSelect } from '@wordpress/data';
+import { useEffect, useRef, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { addQueryArgs } from '@wordpress/url';
+
+/**
+ * Internal dependencies
+ */
+import usePostMeta from '../../store/hooks/use-post-meta';
+import './style.css';
+
+const ForwardButton = ( { children, disabled, onClick } ) => (
+	<Button className="pattern-modal-publish__button" isPrimary disabled={ disabled } onClick={ onClick }>
+		{ children }
+	</Button>
+);
+
+export default function SubmitModal( { onSuccess } ) {
+	// Get Document default
+	const { postTitle } = useSelect( ( select ) => {
+		return {
+			postTitle: select( editorStore ).getEditedPostAttribute( 'title' ),
+		};
+	} );
+
+	const [ currentPage, setCurrentPage ] = useState( 0 );
+	const [ title, setTitle ] = useState( postTitle );
+	const [ description, setDescription ] = usePostMeta( 'wpop_description', '' );
+	const [ selectedCategories, setSelectedCategories ] = useState( [] );
+	const [ categories, setCategories ] = useState( [] );
+	const container = useRef();
+
+	const goBack = () => {
+		setCurrentPage( currentPage - 1 );
+	};
+
+	const goForward = () => {
+		setCurrentPage( currentPage + 1 );
+		container.current.closest( '[role="dialog"]' ).focus();
+	};
+
+	useEffect( () => {
+		const getCategories = () => {
+			apiFetch( {
+				path: addQueryArgs( '/wp/v2/pattern-categories' ),
+			} ).then( ( res ) => {
+				setCategories(
+					res.map( ( i ) => {
+						return {
+							value: i.slug,
+							label: i.name,
+						};
+					} )
+				);
+			} );
+		};
+
+		getCategories();
+	}, [] );
+
+	const pages = [
+		{
+			content: (
+				<>
+					<TextControl
+						className="pattern-modal__control"
+						label={ __( 'Title', 'wporg-patterns' ) }
+						value={ title }
+						placeholder={ __( 'Name your pattern', 'wporg-patterns' ) }
+						onChange={ setTitle }
+						required={ true }
+					/>
+					<TextareaControl
+						className="pattern-modal__control"
+						label={ __( 'Description', 'wporg-patterns' ) }
+						value={ description }
+						placeholder={ __( 'Describe the output of pattern', 'wporg-patterns' ) }
+						help={ __(
+							'The description is used to help users of assistive technology better understand the contents of your pattern.',
+							'wporg-patterns'
+						) }
+						onChange={ setDescription }
+						required={ true }
+					/>
+				</>
+			),
+			footer: (
+				<>
+					<span />
+					<ForwardButton disabled={ ! title.length || ! description.length } onClick={ goForward }>
+						{ __( 'Next', 'wporg-patterns' ) }
+					</ForwardButton>
+				</>
+			),
+		},
+		{
+			content: (
+				<>
+					<fieldset>
+						<legend>{ __( 'Category', 'wporg-patterns' ) }</legend>
+						<p>
+							{ __(
+								'Categories help people find patterns and determines where your pattern will appear in the WordPress Editor.',
+								'wporg-patterns'
+							) }
+						</p>
+						<div className="pattern-modal__checkbox-list">
+							{ categories.map( ( i ) => (
+								<CheckboxControl
+									key={ i.value }
+									label={ i.label }
+									value={ i.value }
+									checked={ selectedCategories.includes( i.value ) }
+									onChange={ ( checked ) => {
+										if ( checked ) {
+											setSelectedCategories( [ ...selectedCategories, i.value ] );
+										} else {
+											setSelectedCategories(
+												selectedCategories.filter( ( cat ) => cat !== i.value )
+											);
+										}
+									} }
+								/>
+							) ) }
+						</div>
+					</fieldset>
+				</>
+			),
+			footer: (
+				<>
+					<Button onClick={ goBack }>{ __( 'Previous', 'wporg-patterns' ) }</Button>
+					<ForwardButton disabled={ ! selectedCategories.length } onClick={ onSuccess }>
+						{ __( 'Finish', 'wporg-patterns' ) }
+					</ForwardButton>
+				</>
+			),
+		},
+	];
+
+	return (
+		<Modal className="pattern-modal pattern-modal-publish" onRequestClose={ onSuccess }>
+			<div className="pattern-modal-publish__page" ref={ container }>
+				<div className="pattern-modal-publish__sidebar">
+					<h3 className="pattern-modal__title pattern-modal__title-sidebar">
+						{ __( 'Publish your pattern', 'wporg-patterns' ) }
+					</h3>
+				</div>
+				<div className="pattern-modal-publish__content">
+					<div>{ pages[ currentPage ].content }</div>
+					<div className="pattern-modal-publish__footer">{ pages[ currentPage ].footer }</div>
+				</div>
+			</div>
+		</Modal>
+	);
+}
---
+++ public_html/wp-content/plugins/pattern-creator/src/components/publish-modal/success.js
@@ -0,0 +1,28 @@
+/**
+ * WordPress dependencies
+ */
+import { Button, Modal } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+export default function PublishedModal() {
+	return (
+		<Modal className="pattern-modal pattern-modal-published">
+			<div className="pattern-modal-published__page">
+				<h3 className="pattern-modal__title">
+					{ __( 'Thank you for sharing your pattern!', 'wporg-patterns' ) }
+				</h3>
+				<p className="pattern-modal__copy">
+					{ __(
+						"Your pattern is pending review. We'll email you when its been published in the public directory.",
+						'wporg-patterns'
+					) }
+				</p>
+				<div className="pattern-modal-published__content">
+					<Button isPrimary>{ __( 'Close', 'wporg-patterns' ) }</Button>
+					<Button className="pattern-modal__link" isLink>{ __( 'Create another pattern', 'wporg-patterns' ) }</Button>
+					<Button className="pattern-modal__link" isLink>{ __( 'View my patterns', 'wporg-patterns' ) }</Button>
+				</div>
+			</div>
+		</Modal>
+	);
+}
