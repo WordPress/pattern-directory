0e7421b01898bcd307aba4d1b1027bb577d7faa4
dufresnesteven <steve.dufresne@automattic.com>
2021-05-31 15:08:42 +0900

    Clean up the jsDoc language.

--- public_html/wp-content/plugins/pattern-creator/src/hooks/use-post-data.js
+++ public_html/wp-content/plugins/pattern-creator/src/hooks/use-post-data.js
@@ -7,9 +7,9 @@ import { store } from '@wordpress/editor';
 /**
  * A hook to get and set a post value.
  *
- * @param {string|Object} property Identifier for a post property to use.
+ * @param {string} property Identifier for a post property to use.
  *
- * @return {Array<*,Function>} A pair of values: the current meta value and a callback to update this meta value.
+ * @return {Array<*,Function>} A pair of values: the current property value and a callback to update this value.
  */
 export default function usePostData( property ) {
 	const propValue = useSelect( ( select ) => select( store ).getEditedPostAttribute( property ) );
