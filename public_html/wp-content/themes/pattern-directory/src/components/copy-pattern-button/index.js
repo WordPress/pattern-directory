f8d9d1737292e726c473c1e072895215a3883f34
Kelly Dwan <ryelle@users.noreply.github.com>
2021-06-03 14:36:29 -0400

    Patterns: Use CopyPatternButton in thumbnail (#119)
    * Patterns: Use CopyPatternButton in thumbnail
    
    This consolidates the copy code, removing the duplicate code from PatternThumbnail
    
    * Fix class names and styles
    
    * Use `lodash.noop` for a default onSuccess

--- public_html/wp-content/themes/pattern-directory/src/components/copy-pattern-button/index.js
+++ public_html/wp-content/themes/pattern-directory/src/components/copy-pattern-button/index.js
@@ -1,21 +1,35 @@
 /**
  * External dependencies
  */
+import classnames from 'classnames';
+import { noop } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
 import { __ } from '@wordpress/i18n';
+import { speak } from '@wordpress/a11y';
 import { Button } from '@wordpress/components';
+import { useEffect, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { copyToClipboard } from '../../utils';
 
-const CopyPatternButton = ( { onSuccess } ) => {
-	const handleClick = ( { target } ) => {
+const CopyPatternButton = ( { isSmall = false, onSuccess = noop, content } ) => {
+	const [ copied, setCopied ] = useState( false );
+
+	if ( ! content ) {
 		// Grab the pattern markup from hidden input
 		const blockData = document.getElementById( 'block-data' );
-		const blockPattern = JSON.parse( decodeURIComponent( blockData.value ) );
+		content = JSON.parse( decodeURIComponent( blockData.value ) );
+	}
 
-		const success = copyToClipboard( blockPattern );
+	const handleClick = ( { target } ) => {
+		const success = copyToClipboard( content );
+
+		setCopied( success );
 
 		// Make sure we reset focus in case it was lost in the 'copy' command.
 		target.focus();
@@ -27,9 +41,32 @@ const CopyPatternButton = ( { onSuccess } ) => {
 		}
 	};
 
+	useEffect( () => {
+		if ( ! copied ) {
+			return;
+		}
+
+		speak( __( 'Copied pattern to clipboard.', 'wporg-patterns' ) );
+
+		const timer = setTimeout( () => setCopied( false ), 20000 );
+		return () => {
+			clearTimeout( timer );
+		};
+	}, [ copied ] );
+
+	let label = __( 'Copy Pattern', 'wporg-patterns' );
+	if ( isSmall ) {
+		label = copied ? __( 'Copied', 'wporg-patterns' ) : __( 'Copy', 'wporg-patterns' );
+	}
+
+	const classes = classnames( {
+		'pattern__copy-button': true,
+		'is-small-label': isSmall,
+	} );
+
 	return (
-		<Button isPrimary onClick={ handleClick }>
-			{ __( 'Copy Pattern', 'wporg-patterns' ) }
+		<Button className={ classes } isPrimary onClick={ handleClick }>
+			{ label }
 		</Button>
 	);
 };
--- public_html/wp-content/themes/pattern-directory/src/components/pattern-thumbnail/index.js
+++ public_html/wp-content/themes/pattern-directory/src/components/pattern-thumbnail/index.js
@@ -1,46 +1,16 @@
 /**
  * WordPress dependencies
  */
-import { speak } from '@wordpress/a11y';
-import { __, sprintf } from '@wordpress/i18n';
-import { Button, Disabled } from '@wordpress/components';
-import { useEffect, useState } from '@wordpress/element';
+import { Disabled } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import FavoriteButton from '../favorite-button';
+import CopyPatternButton from '../copy-pattern-button';
 import Canvas from './canvas';
-import { copyToClipboard } from '../../utils';
 
 function PatternThumbnail( { pattern } ) {
-	const [ copied, setCopied ] = useState( false );
-
-	const handleCopy = () => {
-		const result = copyToClipboard( pattern.pattern_content );
-
-		setCopied( result );
-	};
-
-	useEffect( () => {
-		if ( ! copied ) {
-			return;
-		}
-
-		speak(
-			sprintf(
-				/* translators: %s: pattern title. */
-				__( 'Copied %s pattern to clipboard.', 'wporg-patterns' ),
-				pattern.title.rendered
-			)
-		);
-
-		const timer = setTimeout( () => setCopied( false ), 20000 );
-		return () => {
-			clearTimeout( timer );
-		};
-	}, [ copied ] );
-
 	return (
 		<div className="pattern-grid__pattern">
 			<a href={ pattern.link } rel="bookmark">
@@ -52,9 +22,7 @@ function PatternThumbnail( { pattern } ) {
 			<div className="pattern-grid__actions">
 				<h2 className="pattern-grid__title">{ pattern.title.rendered }</h2>
 				<FavoriteButton showLabel={ false } patternId={ pattern.id } />
-				<Button className="pattern__copy-button is-small" isPrimary onClick={ handleCopy }>
-					{ copied ? __( 'Copied!', 'wporg-patterns' ) : __( 'Copy', 'wporg-patterns' ) }
-				</Button>
+				<CopyPatternButton isSmall={ true } content={ pattern.pattern_content } />
 			</div>
 		</div>
 	);
