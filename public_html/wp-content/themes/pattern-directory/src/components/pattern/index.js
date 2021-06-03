7779884198a58af01cd8bc8041ba9e108c7cb8a1
Kelly Dwan <ryelle@users.noreply.github.com>
2021-06-03 10:44:31 -0400

    Move assorted pattern-details components into a single `Pattern` component

---
+++ public_html/wp-content/themes/pattern-directory/src/components/pattern/index.js
@@ -0,0 +1,24 @@
+/**
+ * Internal dependencies
+ */
+import PatternPreview from '../pattern-preview';
+import PatternPreviewActions from '../pattern-preview-actions';
+import ReportPatternButton from '../report-pattern-button';
+
+const Pattern = ( { content, postId, userHasReported, loggedIn } ) => {
+	return (
+		<>
+			<PatternPreviewActions postId={ postId } />
+			<PatternPreview blockContent={ content } />
+			<div className="pattern__meta">
+				<ReportPatternButton
+					userHasReported={ userHasReported === 'true' }
+					loggedIn={ loggedIn === 'true' }
+					postId={ postId }
+				/>
+			</div>
+		</>
+	);
+};
+
+export default Pattern;
