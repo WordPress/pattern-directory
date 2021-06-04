53b4aed1b62b05f8c61798ddc55ea0e02e1c765d
Kelly Dwan <ryelle@users.noreply.github.com>
2021-06-04 15:09:38 -0400

    Single Patterns: Update pattern preview and header (#121)
    * Move assorted pattern-details components into a single `Pattern` component
    
    * Fix display of notice action
    
    * Style new components
    
    * Add a width control for the preview
    
    * Add extra 40px for the drag-handles
    
    * remove the categories label
    
    * Fix favoriting & update styles
    
    * Update header font weight
    
    * Use `some` instead of `map().indexOf`

--- public_html/wp-content/themes/pattern-directory/src/index.js
+++ public_html/wp-content/themes/pattern-directory/src/index.js
@@ -6,50 +6,24 @@ import { render } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import PatternPreview from './components/pattern-preview';
-import PatternPreviewActions from './components/pattern-preview-actions';
-import ReportPatternButton from './components/report-pattern-button';
+import Pattern from './components/pattern';
 import Patterns from './components/patterns';
+
+// Load the grid into the awaiting preview container.
+const gridContainer = document.getElementById( 'patterns__container' );
+if ( gridContainer ) {
+	render( <Patterns />, gridContainer );
+}
 
 // Load the preview into any awaiting preview container.
 const previewContainers = document.querySelectorAll( '.pattern-preview__container' );
 for ( let i = 0; i < previewContainers.length; i++ ) {
 	const container = previewContainers[ i ];
 	const blockContent = JSON.parse( decodeURIComponent( container.innerText ) );
-	// Use `wp.blocks.parse` to convert HTML to block objects (for use in editor), if needed.
+	const props = container.dataset;
 
-	render( <PatternPreview blockContent={ blockContent } />, container, () => {
+	render( <Pattern { ...props } content={ blockContent } />, container, () => {
 		// This callback is called after the render to unhide the container.
 		container.hidden = false;
 	} );
-}
-
-// Load the preview into any awaiting preview container.
-const gridContainer = document.getElementById( 'patterns__container' );
-if ( gridContainer ) {
-	render( <Patterns />, gridContainer );
-}
-
-// Load the pattern preview actions
-const patternActionsContainer = document.getElementById( 'pattern-actions' );
-if ( patternActionsContainer ) {
-	render(
-		<PatternPreviewActions patternId={ Number( patternActionsContainer.dataset.id ) } />,
-		patternActionsContainer
-	);
-}
-
-// Load report button
-const patternReportContainer = document.getElementById( 'pattern-report' );
-if ( patternReportContainer ) {
-	const { loggedIn, postId, userHasReported } = patternReportContainer.dataset;
-
-	render(
-		<ReportPatternButton
-			userHasReported={ userHasReported === 'true' }
-			loggedIn={ loggedIn === 'true' }
-			postId={ postId }
-		/>,
-		patternReportContainer
-	);
 }
