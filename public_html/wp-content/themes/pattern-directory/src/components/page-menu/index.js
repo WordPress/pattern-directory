9c82bc2fc5deb2ed5c89b8133ce9532ebd00ee85
dufresnesteven <steve.dufresne@automattic.com>
2021-06-16 14:35:54 +0900

    Move breadcrumbs away.

--- public_html/wp-content/themes/pattern-directory/src/components/page-menu/index.js
+++ public_html/wp-content/themes/pattern-directory/src/components/page-menu/index.js
@@ -7,40 +7,12 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import PatternSearch from '../pattern-search';
-import { getCategoryFromPath } from '../../utils';
-import { useRoute } from '../../hooks';
-import { store as patternStore } from '../../store';
-import { useSelect } from '@wordpress/data';
+import Breadcrumbs from './breadcrumbs';
 
 const PageMenu = ( { isLoggedIn } ) => {
-	const { path, update: updatePath } = useRoute();
-	const categorySlug = getCategoryFromPath( path );
-	const category = useSelect( ( select ) => select( patternStore ).getCategoryBySlug( categorySlug ) );
-
-	const handleLinkClick = ( event ) => {
-		event.preventDefault();
-
-		updatePath( event.target.pathname );
-	};
-
 	return (
 		<>
-			<div>
-				<a href={ wporgSiteUrl } onClick={ handleLinkClick } rel="home">
-					{ __( 'All Patterns', 'wporg-patterns' ) }
-				</a>
-				<span className="sep">/</span>
-				{ category && (
-					<>
-						<a href={ wporgSiteUrl } rel="home">
-							{ __( 'Categories', 'wporg-patterns' ) }
-						</a>
-						<span className="sep">/</span>
-					</>
-				) }
-
-				<span>{ category ? category.name : 'Single' }</span>
-			</div>
+			<Breadcrumbs />
 			<nav id="site-navigation" className="main-navigation" role="navigation">
 				<button
 					className="menu-toggle dashicons dashicons-arrow-down-alt2"
