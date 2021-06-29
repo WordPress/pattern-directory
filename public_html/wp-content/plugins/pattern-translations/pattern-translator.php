0c0cfdd7b364d84e02bdaf09330a1795deeb94f6
Dion Hulse <dion@wordpress.org>
2021-06-29 04:36:51 +0000

    Remove unneeded functionality

--- public_html/wp-content/plugins/pattern-translations/pattern-translator.php
+++ public_html/wp-content/plugins/pattern-translations/pattern-translator.php
@@ -8,10 +8,7 @@
 namespace WordPressdotorg\Pattern_Translations;
 
 require __DIR__ . '/init.php';
-require __DIR__ . '/pattern-meta.php';
-require __DIR__ . '/pattern-media.php';
 require __DIR__ . '/pattern-stores.php';
-require __DIR__ . '/pattern-cache.php';
 require __DIR__ . '/parser.php';
 require __DIR__ . '/i18n.php';
 require __DIR__ . '/makepot.php';
