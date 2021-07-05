5d70541d3169fd8c2fcd4e2f81bc32238261b3b4
Kelly Dwan <ryelle@users.noreply.github.com>
2021-07-05 18:49:23 -0400

    Pattern favorites: Save a cached value of how many times a pattern has been favorited.
    This is saved to the post meta for the given post, since we will want to order by this value.

--- public_html/wp-content/plugins/pattern-directory/includes/favorite.php
+++ public_html/wp-content/plugins/pattern-directory/includes/favorite.php
@@ -84,3 +84,81 @@ function get_favorites( $user = 0 ) {
 
 	return array_map( 'absint', $favorites );
 }
+
+/**
+ * Get the cached count of how many times this pattern has been favorited.
+ *
+ * @param int|WP_Post $post The pattern to check.
+ * @return integer
+ */
+function get_favorite_count( $post = 0 ) {
+	$post = get_block_pattern( $post );
+	if ( ! $post ) {
+		return false;
+	}
+
+	return absint( get_post_meta( $post->ID, META_KEY, true ) );
+}
+
+/**
+ * Get a count of how many times this pattern has been favorited, directly from the users table.
+ *
+ * @param int|WP_Post $post The pattern to check.
+ * @return integer
+ */
+function get_raw_favorite_count( $post = 0 ) {
+	global $wpdb;
+	$post = get_block_pattern( $post );
+	if ( ! $post ) {
+		return false;
+	}
+	$count = $wpdb->get_var( $wpdb->prepare(
+		"SELECT COUNT(*)
+			FROM {$wpdb->usermeta}
+			WHERE meta_key=%s
+			AND meta_value=%d",
+		META_KEY,
+		$post->ID
+	) );
+
+	return absint( $count );
+}
+
+/**
+ * Update a given post's favorite count cache.
+ *
+ * @param mixed $value The post ID.
+ */
+function update_favorite_cache( $value ) {
+	if ( ! is_numeric( $value ) ) {
+		return;
+	}
+
+	$count = get_raw_favorite_count( $value );
+	update_post_meta( $value, META_KEY, $count );
+}
+
+/**
+ * Trigger the update of favorite count when a user favorites or unfavorites a pattern.
+ *
+ * @param int    $mid         The meta ID.
+ * @param int    $object_id   ID of the object metadata is for.
+ * @param string $meta_key    Metadata key.
+ * @param mixed  $_meta_value Metadata value. Serialized if non-scalar.
+ */
+function trigger_favorite_cache_update( $mid, $object_id, $meta_key, $_meta_value ) {
+	if ( META_KEY !== $meta_key ) {
+		return;
+	}
+
+	// This value can be an array in the delete action, so walk through each unique value and refresh the cache.
+	if ( is_array( $_meta_value ) ) {
+		$_meta_value = array_unique( $_meta_value );
+		array_walk( $_meta_value, __NAMESPACE__ . '\update_favorite_cache' );
+		return;
+	}
+
+	update_favorite_cache( $_meta_value );
+}
+add_action( 'added_user_meta', __NAMESPACE__ . '\trigger_favorite_cache_update', 10, 4 );
+add_action( 'deleted_user_meta', __NAMESPACE__ . '\trigger_favorite_cache_update', 10, 4 );
