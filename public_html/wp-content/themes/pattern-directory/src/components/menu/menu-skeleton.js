7a7b4c65651337d3e26db17e50d1d668a221c5cf
Steven Dufresne <steve.dufresne@automattic.com>
2021-07-05 13:28:59 +0900

    Theme: Update Pattern Grid menu loading (#254)

---
+++ public_html/wp-content/themes/pattern-directory/src/components/menu/menu-skeleton.js
@@ -0,0 +1,23 @@
+/**
+ * Internal dependencies
+ */
+import { Skeleton, SkeletonWrapper } from '../skeleton';
+
+/**
+ * Module constants
+ */
+const PLACEHOLDER_COUNT = 7;
+
+const MenuSkeleton = () => {
+	return (
+		<SkeletonWrapper style={ { flexDirection: 'row' } }>
+			{ Array( PLACEHOLDER_COUNT )
+				.fill()
+				.map( ( val, idx ) => (
+					<Skeleton key={ idx } height="1.25rem" width="5rem" marginRight="1rem" />
+				) ) }
+		</SkeletonWrapper>
+	);
+};
+
+export default MenuSkeleton;
