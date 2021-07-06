d0a1679d4974501ac4a016ca566b02d14ac54244
Kelly Dwan <ryelle@users.noreply.github.com>
2021-07-06 17:34:27 -0400

    Theme: Add RTL build step, fix issues in RTL sites (#267)
    * Add RTLCSS process
    
    * Fix the preview alignment when site is RTL
    
    * Fix the preview drag handles
    
    * Add dashicons arrow swap plugin

--- public_html/wp-content/themes/pattern-directory/Gruntfile.js
+++ public_html/wp-content/themes/pattern-directory/Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function( grunt ) {
+module.exports = function ( grunt ) {
 	const isChild = 'wporg' !== grunt.file.readJSON( 'package.json' ).name;
 	const defaultWebpackConfig = require( '@wordpress/scripts/config/webpack.config' );
 
@@ -6,7 +6,7 @@ module.exports = function( grunt ) {
 		const files = {};
 		const components = [ 'settings', 'tools', 'generic', 'base', 'objects', 'components', 'utilities' ];
 
-		components.forEach( function( component ) {
+		components.forEach( function ( component ) {
 			const paths = [
 				'../wporg/css/' + component + '/**/*.scss',
 				'!../wporg/css/' + component + '/_' + component + '.scss',
@@ -21,6 +21,42 @@ module.exports = function( grunt ) {
 		} );
 
 		return files;
+	};
+
+	const rtlcssPluginSwapDashiconArrows = {
+		name: 'swap-dashicons-left-right-arrows',
+		priority: 10,
+		directives: {
+			control: {},
+			value: [],
+		},
+		processors: [
+			{
+				expr: /content/im,
+				action: function ( prop, value ) {
+					if ( value === '"\\f141"' ) {
+						// replace dashicons-arrow-left with -right.
+						value = '"\\f139"';
+					} else if ( value === '"\\f340"' ) {
+						// replace dashicons-arrow-left-alt with -right-alt.
+						value = '"\\f344"';
+					} else if ( value === '"\\f341"' ) {
+						// replace dashicons-arrow-left-alt2 with -right-alt2.
+						value = '"\\f345"';
+					} else if ( value === '"\\f139"' ) {
+						// replace dashicons-arrow-right with -left.
+						value = '"\\f141"';
+					} else if ( value === '"\\f344"' ) {
+						// replace dashicons-arrow-right-alt with -left-alt.
+						value = '"\\f340"';
+					} else if ( value === '"\\f345"' ) {
+						// replace dashicons-arrow-right-alt2 with -left-alt2.
+						value = '"\\f341"';
+					}
+					return { prop, value };
+				},
+			},
+		],
 	};
 
 	grunt.initConfig( {
@@ -39,6 +75,13 @@ module.exports = function( grunt ) {
 			},
 			dist: {
 				src: 'css/style.css',
+			},
+			rtl: {
+				options: {
+					processors: [ require( 'rtlcss' )( null, [ rtlcssPluginSwapDashiconArrows ] ) ],
+				},
+				src: 'css/style.css',
+				dest: 'css/style-rtl.css',
 			},
 		},
 
