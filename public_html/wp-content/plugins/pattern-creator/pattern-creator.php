050768aa9dc64b2ce135d1551327c6933c7552cd
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2021-07-07 10:02:30 -0700

    Specify correct textdomain in plugin's headers

--- public_html/wp-content/plugins/pattern-creator/pattern-creator.php
+++ public_html/wp-content/plugins/pattern-creator/pattern-creator.php
@@ -5,7 +5,7 @@
  * Version: 1.0.0
  * Requires at least: 5.5
  * Author: WordPress Meta Team
- * Text Domain: wporg-pattern-creator
+ * Text Domain: wporg-patterns
  * License: GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
