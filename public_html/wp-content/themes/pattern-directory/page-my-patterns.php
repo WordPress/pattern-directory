2fd4bdd27f09fd3a53a2a36982260017c379b338
Steven Dufresne <steve.dufresne@automattic.com>
2021-07-16 00:17:24 +0900

    My Patterns: Turn off the create new pattern button. (#289)

--- public_html/wp-content/themes/pattern-directory/page-my-patterns.php
+++ public_html/wp-content/themes/pattern-directory/page-my-patterns.php
@@ -14,7 +14,7 @@ get_header();
 
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<a href="<?php echo esc_url( home_url( '/new-pattern' ) ); ?>" class="button button-outline"><?php esc_html_e( 'Create a new pattern', 'wporg-patterns' ); ?></a>
+		<!-- <a href="<?php echo esc_url( home_url( '/new-pattern' ) ); ?>" class="button button-outline"><?php esc_html_e( 'Create a new pattern', 'wporg-patterns' ); ?></a> -->
 	</header><!-- .entry-header -->
 
 	<div id="my-patterns__container"></div>
