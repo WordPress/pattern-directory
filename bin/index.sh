8dfb8bc8b9416e0194d1beb118490e9d47bffa58
Kelly Dwan <ryelle@users.noreply.github.com>
2021-11-22 12:16:55 -0500

    Tools: Update setup script
    Move the locales import up, to prevent issues later in wp-cli commands. Create an editor user for multi-user testing.

--- bin/index.sh
+++ bin/index.sh
@@ -4,6 +4,7 @@
 set -e
 
 # Install dependencies
+composer update
 yarn
 
 # Build the project
@@ -13,19 +14,24 @@ yarn workspaces run build
 yarn wp-env start --update
 
 # Update wp configs
-yarn wp-env run cli wp config set JETPACK_DEV_DEBUG true
-yarn wp-env run cli wp config set WPORG_SANDBOXED true
+yarn wp-env run cli "wp config set JETPACK_DEV_DEBUG true"
+yarn wp-env run cli "wp config set WPORG_SANDBOXED true"
+
+# Create the table for locales
+yarn wp-env run cli "wp db import wp-content/uploads/data/wporg_locales.sql"
 
 # Activate plugins
-yarn wp-env run cli wp plugin activate gutenberg
-yarn wp-env run cli wp plugin activate pattern-directory/bootstrap.php
-yarn wp-env run cli wp plugin activate pattern-creator
+yarn wp-env run cli "wp plugin activate wordpress-importer"
+yarn wp-env run cli "wp plugin activate gutenberg"
+yarn wp-env run cli "wp plugin activate pattern-directory/bootstrap.php"
+yarn wp-env run cli "wp plugin activate pattern-creator"
 
 # Activate theme
-yarn wp-env run cli wp theme activate pattern-directory
+yarn wp-env run cli "wp theme activate pattern-directory"
 
 # Change permalinks
-yarn wp-env run cli wp rewrite structure '/%postname%/'
+yarn wp-env run cli "wp rewrite structure '/%postname%/'"
 
-# Import tables
-yarn wp-env run cli wp db import wp-content/uploads/data/wporg_locales.sql
+# Create editor user
+yarn wp-env run cli "wp user create editor editor@wp.local --role=editor --user_pass=password"
+
