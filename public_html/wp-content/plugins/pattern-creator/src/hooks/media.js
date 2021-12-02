8fd49bb4047ac5f61a2d429cb68c0c125352f3f7
Kelly Dwan <ryelle@users.noreply.github.com>
2021-12-02 13:28:03 -0500

    Pattern Creator: Increase priority on openverse media library.

--- public_html/wp-content/plugins/pattern-creator/src/hooks/media.js
+++ public_html/wp-content/plugins/pattern-creator/src/hooks/media.js
@@ -10,4 +10,4 @@ import MediaPlaceholder from '../components/media-placeholder';
 import OpenverseGallery from '../components/openverse';
 
 addFilter( 'editor.MediaPlaceholder', 'wporg/patterns/components/media-upload', () => MediaPlaceholder, 100 );
-addFilter( 'editor.MediaUpload', 'wporg-patterns/openverse-media-upload', () => OpenverseGallery );
+addFilter( 'editor.MediaUpload', 'wporg-patterns/openverse-media-upload', () => OpenverseGallery, 100 );
