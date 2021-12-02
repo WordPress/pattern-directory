eaddc5b4cf3167abe4fd304a369ce6185d103619
Kelly Dwan <ryelle@users.noreply.github.com>
2021-12-02 13:50:24 -0500

    Pattern Creator: Remove media API override
    We're not using the core media library anymore, so we don't need to do this override.

--- public_html/wp-content/plugins/pattern-creator/src/api-middleware/filter-endpoints.js
+++ public_html/wp-content/plugins/pattern-creator/src/api-middleware/filter-endpoints.js
@@ -1,14 +1,9 @@
 // Use a middleware provider to intercept and modify API calls.
 // Short-circuit POST requests, bound queries, allow media, etc.
 export default function ( options, next ) {
-	// Add limits to all GET queries which attempt unbound queries
 	if ( options.path ) {
+		// Add limits to all GET queries which attempt unbound queries
 		options.path = options.path.replace( 'per_page=-1', 'per_page=50' );
-
-		// Load images with the view context, seems to work
-		if ( 0 === options.path.indexOf( '/wp/v2/media' ) ) {
-			options.path = options.path.replace( 'context=edit', 'context=view' );
-		}
 	}
 
 	return next( options );
