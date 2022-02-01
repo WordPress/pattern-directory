8e5528e21055c89f61129033807d1a63395d6504
Kelly Dwan <ryelle@users.noreply.github.com>
2022-02-01 17:45:44 -0500

    Tools: Add mu-plugins repo for global header & footer

--- .wp-env/0-sandbox.php
+++ .wp-env/0-sandbox.php
@@ -2,3 +2,10 @@
 // This file handles special loading of mu-plugins.
 
 require_once __DIR__ . '/pub/locales.php';
+
+if ( ! defined( 'FEATURE_2021_GLOBAL_HEADER_FOOTER' ) ) {
+	define( 'FEATURE_2021_GLOBAL_HEADER_FOOTER', true );
+}
+
+require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/blocks/global-header-footer/blocks.php';
+require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/skip-to/skip-to.php';
