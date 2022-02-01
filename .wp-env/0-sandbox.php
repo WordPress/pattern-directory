32d44e56badc528d0f92c23130f9ba1fbe278177
Kelly Dwan <ryelle@users.noreply.github.com>
2022-02-01 18:18:52 -0500

    Sandbox: Add a definition for `WPORGPATH` constant.

--- .wp-env/0-sandbox.php
+++ .wp-env/0-sandbox.php
@@ -7,5 +7,9 @@ if ( ! defined( 'FEATURE_2021_GLOBAL_HEADER_FOOTER' ) ) {
 	define( 'FEATURE_2021_GLOBAL_HEADER_FOOTER', true );
 }
 
+if ( ! defined( 'WPORGPATH' ) ) {
+	define( 'WPORGPATH', '/not/here/' );
+}
+
 require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/blocks/global-header-footer/blocks.php';
 require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/skip-to/skip-to.php';
