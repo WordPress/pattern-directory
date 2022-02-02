bee5deaa150f8a8af032f0ec68ea67f1d06ed3eb
Kelly Dwan <ryelle@users.noreply.github.com>
2022-02-02 16:18:40 -0500

    Admin Bar: Update New and Edit links to go to the Pattern Creator.
    Fixes #387

--- public_html/wp-content/plugins/pattern-creator/includes/admin.php
+++ public_html/wp-content/plugins/pattern-creator/includes/admin.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace WordPressdotorg\Pattern_Creator\Admin;
+use const WordPressdotorg\Pattern_Directory\Pattern_Post_Type\POST_TYPE;
 
 defined( 'WPINC' ) || die();
 
 add_action( 'admin_menu', __NAMESPACE__ . '\admin_menu' );
 add_action( 'admin_init', __NAMESPACE__ . '\admin_init' );
+add_action( 'admin_bar_menu', __NAMESPACE__ . '\filter_admin_bar_links', 500 ); // 500 to run after all items are added to the menu.
 
 const PAGE_SLUG = 'wporg-pattern-creator';
 const SECTION_NAME = 'wporg-pattern-settings';
@@ -82,4 +84,37 @@ function render_status_field() {
  */
 function render_page() {
 	require_once dirname( __DIR__ ) . '/view/settings.php';
+}
+
+/**
+ * Filter the admin bar links to direct to the Pattern Creator.
+ *
+ * @param WP_Admin_Bar $wp_admin_bar The WP_Admin_Bar instance, passed by reference.
+ */
+function filter_admin_bar_links( $wp_admin_bar ) {
+	// "New Block Pattern" link.
+	$new_pattern = $wp_admin_bar->get_node( 'new-wporg-pattern' );
+	if ( $new_pattern ) {
+		$new_pattern->href = site_url( 'new-pattern/' );
+		$wp_admin_bar->remove_node( $new_pattern->id );
+		$wp_admin_bar->add_node( $new_pattern );
+	}
+
+	// Top-level "+ New" link, if New Block Pattern is the only item.
+	$new_content = $wp_admin_bar->get_node( 'new-content' );
+	if ( $new_content && str_contains( $new_content->href, POST_TYPE ) ) {
+		$new_content->href = site_url( 'new-pattern/' );
+		$wp_admin_bar->remove_node( $new_content->id );
+		$wp_admin_bar->add_node( $new_content );
+	}
+
+	// "Edit Block Pattern" link.
+	if ( is_singular( POST_TYPE ) ) {
+		$edit_pattern = $wp_admin_bar->get_node( 'edit' );
+		if ( $edit_pattern ) {
+			$edit_pattern->href = site_url( 'pattern/' . get_the_ID() . '/edit/' );
+			$wp_admin_bar->remove_node( $edit_pattern->id );
+			$wp_admin_bar->add_node( $edit_pattern );
+		}
+	}
 }
