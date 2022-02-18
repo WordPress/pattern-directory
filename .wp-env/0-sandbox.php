2c65c8b800cb91334af5d682802ed25498c11acf
Kelly Dwan <ryelle@users.noreply.github.com>
2022-02-18 15:54:32 -0500

    Environment: Update mu-plugins loader and remove unnecessary constants

--- .wp-env/0-sandbox.php
+++ .wp-env/0-sandbox.php
@@ -1,15 +1,5 @@
 <?php
 // This file handles special loading of mu-plugins.
 
-require_once __DIR__ . '/pub/locales.php';
-
-if ( ! defined( 'FEATURE_2021_GLOBAL_HEADER_FOOTER' ) ) {
-	define( 'FEATURE_2021_GLOBAL_HEADER_FOOTER', true );
-}
-
-if ( ! defined( 'WPORGPATH' ) ) {
-	define( 'WPORGPATH', '/not/here/' );
-}
-
-require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/blocks/global-header-footer/blocks.php';
-require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/skip-to/skip-to.php';
+require_once WPMU_PLUGIN_DIR . '/pub/locales.php';
+require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/loader.php';
