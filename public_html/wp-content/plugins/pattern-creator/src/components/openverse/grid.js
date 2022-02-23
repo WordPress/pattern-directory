2eb31ae549370e8025b6cc9e407a7f1d1993203d
Steven Dufresne <steve.dufresne@automattic.com>
2022-02-24 08:51:19 +0900

    Center loader in Openverse control. (#401)
    * Center the openverse loader.
    
    * Reorder the css declarations.
    
    * Update classname to match component.

--- public_html/wp-content/plugins/pattern-creator/src/components/openverse/grid.js
+++ public_html/wp-content/plugins/pattern-creator/src/components/openverse/grid.js
@@ -120,7 +120,7 @@ export default function OpenverseGrid( { searchTerm, onClose, onSelect, multiple
 
 	if ( isLoading ) {
 		return (
-			<div>
+			<div className="pattern-openverse__spinner">
 				<Spinner />
 			</div>
 		);
