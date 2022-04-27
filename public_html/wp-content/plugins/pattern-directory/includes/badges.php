79de1edef3d7d19d241b6c82fc1509185fb57492
Dion Hulse <dion@wordpress.org>
2022-04-27 15:58:27 +1000

    Linting fixes, documentation, and a super-cautious extra conditional.

--- public_html/wp-content/plugins/pattern-directory/includes/badges.php
+++ public_html/wp-content/plugins/pattern-directory/includes/badges.php
@@ -11,6 +11,9 @@ add_action( 'transition_post_status', __NAMESPACE__ . '\status_transitions', 10,
 add_action( 'remove_user_from_blog', __NAMESPACE__ . '\remove_user_from_blog', 10, 1 );
 add_action( 'set_user_role', __NAMESPACE__ . '\set_user_role', 10, 2 );
 
+/**
+ * Watch for pattern status changes, and assign (or remove) the Pattern Author badge as appropriate.
+ */
 function status_transitions( $new_status, $old_status, $post ) {
 	$post = get_post( $post );
 
@@ -24,7 +27,7 @@ function status_transitions( $new_status, $old_status, $post ) {
 
 	if ( 'publish' === $new_status ) {
 		assign_badge( 'pattern-author', $post->post_author );
-	} elseif ( 'publish' === $old_status ) {
+	} elseif ( 'publish' === $old_status && 'publish' !== $new_status ) {
 		// If the user has no published patterns, remove the badge.
 		$other_posts = get_posts( [
 			'post_type'   => PATTERN_POST_TYPE,
@@ -41,12 +44,21 @@ function status_transitions( $new_status, $old_status, $post ) {
 	}
 }
 
+/**
+ * Remove the 'Patterns Team' badge from a user when they're removed from the Patterns site.
+ */
 function remove_user_from_blog( $user_id ) {
 	if ( function_exists( 'WordPressdotorg\Profiles\remove_badge' ) ) {
 		remove_badge( 'patterns-team', $user_id );
 	}
 }
 
+/**
+ * Add/Remove the 'Patterns Team' badge from a user when their role changes.
+ *
+ * The badge is added for all roles except for Contributor and Subscriber.
+ * The badge is removed when the role is set to Contributor or Subscriber.
+ */
 function set_user_role( $user_id, $role ) {
 	if ( ! function_exists( 'WordPressdotorg\Profiles\assign_badge' ) ) {
 		return;
