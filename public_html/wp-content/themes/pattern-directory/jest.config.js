b6b320cc3b119cfaa6786d9bd4d400466361c9bf
Kelly Dwan <ryelle@users.noreply.github.com>
2022-08-15 15:20:21 -0400

    Tools: Update packages, remove custom jest configuration
    The Jest config was a workaround for this issue: https://github.com/WordPress/gutenberg/issues/43132 — now that this is fixed in @wordpress/scripts 23.7.1, it's not necessary.

--- public_html/wp-content/themes/pattern-directory/jest.config.js
+++
@@ -1,8 +0,0 @@
-const defaultConfig = require( '@wordpress/scripts/config/jest-unit.config' );
-
-const config = {
-	...defaultConfig,
-	transformIgnorePatterns: [ 'node_modules/(?!(is-plain-obj))' ],
-};
-
-module.exports = config;
