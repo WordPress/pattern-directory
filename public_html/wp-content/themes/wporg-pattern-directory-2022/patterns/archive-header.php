606effac7dcccb20af66497951b8ef30e5c4b428
Kelly Dwan <ryelle@users.noreply.github.com>
2022-09-15 15:51:28 -0400

    Add a pattern for the archive header

---
+++ public_html/wp-content/themes/wporg-pattern-directory-2022/patterns/archive-header.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Archive Header
+ * Slug: wporg-pattern-directory-2022/archive-header
+ * Inserter: no
+ */
+
+?>
+<!-- wp:group {"layout":{"type":"constrained"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"backgroundColor":"blueberry-1","textColor":"white"} -->
+<div class="wp-block-group alignfull has-white-color has-blueberry-1-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group -->
+<div class="wp-block-group"><!-- wp:site-title {"fontSize":"heading-1"} /-->
+
+<!-- wp:paragraph {"className":"is-style-short-text"} -->
+<p class="is-style-short-text">Add a beautifully designed, ready to go layout to any WordPress site with a simple copy/paste.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search patterns","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /-->
+
+<!-- wp:navigation {"ref":8884} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"right","id":143277,"width":392,"height":327,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image alignright size-full is-resized"><img src="http://localhost:8888/wp-content/uploads/2022/09/masthead-bg.png" alt="" class="wp-image-143277" width="392" height="327"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
