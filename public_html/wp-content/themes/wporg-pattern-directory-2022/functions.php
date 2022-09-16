b54d4c32910bc7f9323282089b9742ff69896fc6
Kelly Dwan <ryelle@users.noreply.github.com>
2022-09-16 12:13:19 -0400

    Show a grid of patterns on the homepage & use the category archives

--- public_html/wp-content/themes/wporg-pattern-directory-2022/functions.php
+++ public_html/wp-content/themes/wporg-pattern-directory-2022/functions.php
@@ -1,11 +1,17 @@
 <?php
 
 namespace WordPressdotorg\Theme\Pattern_Directory_2022;
+use const WordPressdotorg\Pattern_Directory\Pattern_Post_Type\POST_TYPE;
+
+// Block files
+require_once( __DIR__ . '/src/pattern-thumbnail/index.php' );
 
 /**
  * Actions and filters.
  */
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+add_filter( 'query_loop_block_query_vars', __NAMESPACE__ . '\update_query_loop_vars', 10, 3 );
+add_action( 'pre_get_posts', __NAMESPACE__ . '\pre_get_posts' );
 
 /**
  * Enqueue scripts and styles.
@@ -20,4 +26,52 @@ function enqueue_assets() {
 		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
 		filemtime( __DIR__ . '/style.css' )
 	);
+}
+
+/**
+ *
+ * @param array    $query Array containing parameters for `WP_Query` as parsed by the block context.
+ * @param WP_Block $block Block instance.
+ * @param int      $page  Current query's page.
+ */
+function update_query_loop_vars( $query, $block, $page ) {
+	if ( isset( $query['post_type']  ) && 'wporg-pattern' === $query['post_type'] ) {
+		$query['posts_per_page'] = 18;
+
+		// The `orderby_locale` meta_query will be transformed into a query orderby by Pattern_Post_Type\filter_orderby_locale().
+		$query['meta_query'] = array(
+			'orderby_locale' => array(
+				'key'     => 'wpop_locale',
+				'compare' => 'IN',
+				// Order in value determines result order
+				'value'   => array( get_locale(), 'en_US' ),
+			),
+		);
+	}
+
+	return $query;
+}
+
+/**
+ *
+ * @param \WP_Query $query The WordPress Query object.
+ */
+function pre_get_posts( $query ) {
+	if ( is_admin() || ! $query->is_main_query() ) {
+		return;
+	}
+
+	if ( $query->is_tax( 'wporg-pattern-category' ) ) {
+		$query->set( 'posts_per_page', 18 );
+
+		// The `orderby_locale` meta_query will be transformed into a query orderby by Pattern_Post_Type\filter_orderby_locale().
+		$query->set( 'meta_query', array(
+			'orderby_locale' => array(
+				'key'     => 'wpop_locale',
+				'compare' => 'IN',
+				// Order in value determines result order
+				'value'   => array( get_locale(), 'en_US' ),
+			),
+		) );
+	}
 }
---
+++ public_html/wp-content/themes/wporg-pattern-directory-2022/patterns/pattern-grid.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Pattern Grid
+ * Slug: wporg-pattern-directory-2022/pattern-grid
+ * Inserter: no
+ */
+
+?>
+<!-- wp:query {"queryId":1,"query":{"inherit":true},"displayLayout":{"type":"flex","columns":3}} -->
+<div class="wp-block-query"><!-- wp:post-template -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","right":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|10"}}}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)"><!-- wp:wporg/pattern-thumbnail /-->
+
+<!-- wp:post-title {"isLink":true,"fontSize":"small","fontFamily":"inter"} /-->
+
+<!-- wp:post-author {"avatarSize":24} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph -->
+<p>No patterns found</p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:query -->
