74b43673081a9b89dc3f2acee902cb8062dd3e96
Kelly Dwan <ryelle@users.noreply.github.com>
2022-09-23 12:35:52 -0400

    Move CSS out of the HTML itself to add a comment

--- public_html/wp-content/plugins/pattern-directory/views/view.php
+++ public_html/wp-content/plugins/pattern-directory/views/view.php
@@ -5,22 +5,29 @@
 
 namespace WordPressdotorg\Pattern_Directory;
 
-/*
- * Get the template HTML.
- * This needs to run before <head> so that blocks can add scripts and styles in wp_head().
- */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<?php wp_head(); ?>
+	<style type='text/css'>
+		/* Vertically center the pattern in the viewport. */
+		.wp-site-blocks {
+			display:flex;
+			align-items:center;
+			min-height:100vh;
+		}
+		.entry-content {
+			width:100%;
+		}
+	</style>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<div class="wp-site-blocks" style="display:flex;align-items:center;min-height:100vh;">
-	<div class="entry-content" style="width:100%">
+<div class="wp-site-blocks">
+	<div class="entry-content">
 		<?php the_content(); ?>
 	</div>
 </div>
