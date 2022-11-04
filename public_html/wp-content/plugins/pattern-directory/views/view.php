cdf9c6bc179a26d8433c3c7b7a21b8bc55b1d842
Kelly Dwan <ryelle@users.noreply.github.com>
2022-11-04 12:00:55 -0400

    Override the default cover color for white background covers

--- public_html/wp-content/plugins/pattern-directory/views/view.php
+++ public_html/wp-content/plugins/pattern-directory/views/view.php
@@ -33,6 +33,13 @@ remove_action( 'wp_footer', 'stats_footer', 101 );
 			width: 100%;
 			max-height: 200px;
 		}
+		/*
+		 * Workaround for placeholder color when used in a white-background cover block.
+		 * TT1 sets the color in covers to white, which makes the placeholder invisible.
+		 */
+		.wp-block-cover__background.has-white-background-color + .wp-block-cover__inner-container {
+			color: var(--global--color-primary);
+		}
 	</style>
 </head>
 
