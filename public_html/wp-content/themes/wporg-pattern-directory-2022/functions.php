fe67dd89a074b8e6ab616fa4e9a41a027d9e8bac
Kelly Dwan <ryelle@users.noreply.github.com>
2022-11-15 16:42:36 -0500

    Try dynamic template parts

--- public_html/wp-content/themes/wporg-pattern-directory-2022/functions.php
+++ public_html/wp-content/themes/wporg-pattern-directory-2022/functions.php
@@ -4,6 +4,7 @@ namespace WordPressdotorg\Theme\Pattern_Directory_2022;
 use const WordPressdotorg\Pattern_Directory\Pattern_Post_Type\POST_TYPE;
 use function WordPressdotorg\Theme\Pattern_Directory_2022\Order_Dropdown_Block\get_orderby_args;
 use function WordPressdotorg\Pattern_Directory\Favorite\{get_favorites};
+use WP_Block_Supports, WP_Block_Template;
 
 // Block files
 require_once( __DIR__ . '/src/blocks/copy-button/index.php' );
@@ -20,6 +21,8 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
 add_filter( 'query_loop_block_query_vars', __NAMESPACE__ . '\update_query_loop_vars', 10, 3 );
 add_action( 'pre_get_posts', __NAMESPACE__ . '\pre_get_posts' );
 add_filter( 'wp_get_nav_menu_items', __NAMESPACE__ . '\contextualize_category_menu_links', 10, 3 );
+add_action( 'render_block_core/template-part', __NAMESPACE__ . '\render_block_template_part_php_file', 10, 3 );
+add_filter( 'pre_get_block_template', __NAMESPACE__ . '\get_block_template_php_file', 10, 3 );
 
 add_action(
 	'init',
@@ -195,4 +198,142 @@ function contextualize_category_menu_links( $items, $menu, $args ) {
 		}
 	}
 	return $items;
+}
+
+/**
+ * Update the template part block to render a PHP file.
+ *
+ * @param string   $block_content The block content.
+ * @param array    $block         The full block, including name and attributes.
+ * @param WP_Block $instance      The block instance.
+ *
+ * @return string Updated block content.
+ */
+function render_block_template_part_php_file( $block_content, $block, $instance ) {
+	$file_path = get_template_part_php_file_path( $block['attrs']['slug'] );
+
+	if ( ! $file_path || ! file_exists( $file_path ) ) {
+		return $block_content;
+	}
+
+	$content = get_template_part_php_file_contents( $file_path );
+
+	$area = WP_TEMPLATE_PART_AREA_UNCATEGORIZED;
+	if ( empty( $attributes['tagName'] ) ) {
+		$defined_areas = get_allowed_block_template_part_areas();
+		$area_tag      = 'div';
+		foreach ( $defined_areas as $defined_area ) {
+			if ( $defined_area['area'] === $area && isset( $defined_area['area_tag'] ) ) {
+				$area_tag = $defined_area['area_tag'];
+			}
+		}
+		$html_tag = $area_tag;
+	} else {
+		$html_tag = esc_attr( $attributes['tagName'] );
+	}
+
+	// Required to prevent `block_to_render` from being null in `get_block_wrapper_attributes`.
+	$parent = WP_Block_Supports::$block_to_render;
+	WP_Block_Supports::$block_to_render = $block;
+	$wrapper_attributes = get_block_wrapper_attributes();
+	WP_Block_Supports::$block_to_render = $parent;
+	return "<$html_tag $wrapper_attributes>" . str_replace( ']]>', ']]&gt;', $content ) . "</$html_tag>";
+}
+
+/**
+ * Shortcut the template part request to load content from a PHP file, if it exists.
+ *
+ * This is used by the API, and therefore in the site editor, `render_block_template_part_php_file`
+ * is used for the frontend display.
+ *
+ * @param WP_Block_Template|null $block_template Return block template object to short-circuit the default query,
+ *                                               or null to allow WP to run its normal queries.
+ * @param string                 $id             Template unique identifier (example: theme_slug//template_slug).
+ * @param string                 $template_type  Template type: `'wp_template'` or '`wp_template_part'`.
+ *
+ * @return WP_Block_Template|null The PHP template content.
+ */
+function get_block_template_php_file( $block_template, $id, $template_type ) {
+	// Currently, the PHP loader only works for template parts.
+	if ( 'wp_template_part' === $template_type ) {
+		$parts = explode( '//', $id, 2 );
+		if ( count( $parts ) < 2 ) {
+			return $block_template;
+		}
+		// This provides both `theme` and `slug`, but theme should always be the
+		// current theme. It's not necessary for the file path as it will fall
+		// back correctly.
+		list( $theme, $slug ) = $parts;
+		$file_path = get_template_part_php_file_path( $slug );
+		if ( ! $file_path ) {
+			return $block_template;
+		}
+
+		$content = get_template_part_php_file_contents( $file_path, true );
+
+		$block_template                 = new WP_Block_Template();
+		$block_template->id             = $theme . '//' . $slug;
+		$block_template->theme          = $theme;
+		$block_template->content        = $content;
+		$block_template->slug           = $slug;
+		$block_template->source         = 'theme';
+		$block_template->type           = $template_type;
+		$block_template->title          = $slug;
+		$block_template->status         = 'publish';
+		$block_template->has_theme_file = true;
+		$block_template->is_custom      = true;
+		$block_template->area           = WP_TEMPLATE_PART_AREA_UNCATEGORIZED;
+	}
+
+	return $block_template;
+}
+
+/**
+ * Get a full file path from a theme (parent or child).
+ *
+ * @param string $slug The file slug.
+ *
+ * @return string|false A full file path, or false if not found.
+ */
+function get_template_part_php_file_path( $slug ) {
+	$parent_theme_folders        = get_block_theme_folders( get_template() );
+	$child_theme_folders         = get_block_theme_folders( get_stylesheet() );
+	$child_theme_part_file_path  = get_theme_file_path( '/' . $child_theme_folders['wp_template_part'] . '/' . $slug . '.php' );
+	$parent_theme_part_file_path = get_theme_file_path( '/' . $parent_theme_folders['wp_template_part'] . '/' . $slug . '.php' );
+	$template_part_file_path     = 0 === validate_file( $slug ) && file_exists( $child_theme_part_file_path ) ? $child_theme_part_file_path : $parent_theme_part_file_path;
+	if ( 0 === validate_file( $slug ) && file_exists( $template_part_file_path ) ) {
+		return $template_part_file_path;
+	}
+	return false;
+}
+
+/**
+ * Get the rendered contents of a given PHP file (as plain HTML).
+ *
+ * @param string $file_path Full path to file.
+ * @param string $raw       True for raw block content, false to parse the blocks.
+ *
+ * @return string The file contents.
+ */
+function get_template_part_php_file_contents( $file_path, $raw = false ) {
+	// Output buffering to capture the resulting HTML.
+	ob_start();
+	require $file_path;
+	$content = ob_get_clean();
+
+	if ( ! $raw ) {
+		$content = do_blocks( $content );
+		$content = wptexturize( $content );
+		$content = convert_smilies( $content );
+		$content = shortcode_unautop( $content );
+		$content = wp_filter_content_tags( $content );
+		$content = do_shortcode( $content );
+
+		// Handle embeds for block template parts.
+		global $wp_embed;
+		$content = $wp_embed->autoembed( $content );
+
+	}
+
+	return $content;
 }
--- public_html/wp-content/themes/wporg-pattern-directory-2022/parts/archive-header.php
+++ public_html/wp-content/themes/wporg-pattern-directory-2022/parts/archive-header.php
@@ -1,14 +1,3 @@
-<?php
-/**
- * Title: Archive Header
- * Slug: wporg-pattern-directory-2022/archive-header
- * Inserter: no
- */
-
-// @todo Hard-code navigation
-// @todo i18n
-
-?>
 <!-- wp:group {"layout":{"type":"constrained"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}},"backgroundColor":"blueberry-1","textColor":"white"} -->
 <div class="wp-block-group alignfull has-white-color has-blueberry-1-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
@@ -16,7 +5,7 @@
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:site-title {"fontSize":"heading-1"} /-->
 
 <!-- wp:paragraph {"className":"is-style-short-text"} -->
-<p class="is-style-short-text">Add a beautifully designed, ready to go layout to any WordPress site with a simple copy/paste.</p>
+<p class="is-style-short-text"><?php esc_html_e( 'Add a beautifully designed, ready to go layout to any WordPress site with a simple copy/paste.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search patterns","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /-->
