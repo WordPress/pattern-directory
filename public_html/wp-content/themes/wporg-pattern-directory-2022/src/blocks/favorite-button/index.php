4331593c1cadf3d3e6cc7f9f0144cd2286bff951
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-16 12:00:36 -0500

    Favorite Button: Improve rendering, style, use SVG for heart

--- public_html/wp-content/themes/wporg-pattern-directory-2022/src/blocks/favorite-button/index.php
+++ public_html/wp-content/themes/wporg-pattern-directory-2022/src/blocks/favorite-button/index.php
@@ -7,7 +7,6 @@
  */
 
 namespace WordPressdotorg\Theme\Pattern_Directory_2022\Favorite_Button_Block;
-use function WordPressdotorg\Pattern_Directory\Favorite\{get_favorite_count, is_favorite};
 
 add_action( 'init', __NAMESPACE__ . '\init' );
 
@@ -19,118 +18,5 @@ add_action( 'init', __NAMESPACE__ . '\init' );
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function init() {
-	register_block_type(
-		__DIR__ . '/../../../build/blocks/favorite-button',
-		array(
-			'render_callback' => __NAMESPACE__ . '\render',
-		)
-	);
-}
-
-/**
- * Render the block content.
- *
- * @param array    $attributes Block attributes.
- * @param string   $content    Block default content.
- * @param WP_Block $block      Block instance.
- *
- * @return string Returns the block markup.
- */
-function render( $attributes, $content, $block ) {
-	if ( ! empty( $block->block_type->view_script ) ) {
-		wp_enqueue_script( $block->block_type->view_script );
-		// Move to footer.
-		wp_script_add_data( $block->block_type->view_script, 'group', 1 );
-	}
-	$variant = $attributes[ 'variant' ] ?? 'default';
-
-	$post_id = $block->context['postId'];
-	if ( ! $post_id ) {
-		return '';
-	}
-
-	$user_id = get_current_user_id();
-	
-	if ( 'small' === $variant ) {
-		$button = get_small_button( $post_id, $user_id );
-	} else {
-		if ( ! $user_id ) {
-			return '';
-		}
-		$button = get_button( $post_id, $user_id );
-	}
-
-	$is_favorite = is_favorite( $post_id, $user_id );
-	$classes = [ 'is-style-outline' ];
-	if ( $is_favorite ) {
-		$classes[] = 'is-favorite';
-	}
-	$wrapper_attributes = get_block_wrapper_attributes(
-		array(
-			'class' => implode( ' ', $classes ),
-		)
-	);
-	return sprintf(
-		'<div %1$s data-post-id="%2$s" data-action="%3$s">%4$s</div>',
-		$wrapper_attributes,
-		esc_attr( $post_id ),
-		$is_favorite ? 'remove' : 'add',
-		$button
-	);
-}
-
-function get_button( $post_id, $user_id ) {
-	$add_label = __( 'Add to favorites', 'wporg-patterns' );
-	$remove_label = __( 'Remove from favorites', 'wporg-patterns' );
-	$is_favorite = is_favorite( $post_id, $user_id );
-
-	// Render a disabled button until the JS kicks in.
-	$button = '<button class="wp-block-button__link wp-element-button" disabled="disabled">❤️ ';
-	$button .= '<span class="wp-block-wporg-favorite-button__label">';
-	$button .= $is_favorite ? $remove_label : $add_label;
-	$button .= '</span>';
-	$button .= '</button>';
-
-	return $button;
-}
-
-function get_small_button( $post_id, $user_id ) {
-	$add_label = __( 'Add to favorites', 'wporg-patterns' );
-	$remove_label = __( 'Remove from favorites', 'wporg-patterns' );
-	$is_favorite = is_favorite( $post_id, $user_id );
-	$favorite_count = get_favorite_count( $post_id );
-
-	// Render a disabled button until the JS kicks in.
-	$button = '<button class="wp-block-wporg-favorite-button__button" disabled="disabled">❤️ ';
-
-	if ( ! $user_id ) {
-		$button = '<span class="wp-block-wporg-favorite-button__button" class="">❤️ ';
-	}
-
-	$button .= '<span class="wp-block-wporg-favorite-button__count">';
-	$button .= '<span class="screen-reader-text">';
-	$button .= sprintf(
-		_n(
-			'Favorited %s times',
-			'Favorited %s times',
-			$favorite_count,
-			'wporg-patterns'
-		),
-		$favorite_count
-	);
-	$button .= '</span>';
-	$button .= '<span aria-hidden="true">' . $favorite_count . '</span>';
-	$button .= '</span>';
-
-	$button .= '<span class="wp-block-wporg-favorite-button__label screen-reader-text">';
-	$button .= $is_favorite ? $remove_label : $add_label;
-	$button .= '</span>';
-
-	if ( $user_id ) {
-		$button .= '</button>';
-	} else {
-		$button .= '</span>';
-	}
-
-	return $button;
+	register_block_type( __DIR__ . '/../../../build/blocks/favorite-button' );
 }
---
+++ public_html/wp-content/themes/wporg-pattern-directory-2022/src/blocks/favorite-button/render.php
@@ -0,0 +1,76 @@
+<?php
+
+use function WordPressdotorg\Pattern_Directory\Favorite\{get_favorite_count, is_favorite};
+
+$variant = $attributes[ 'variant' ] ?? 'default';
+
+$post_id = $block->context['postId'];
+if ( ! $post_id ) {
+	return '';
+}
+
+$user_id = get_current_user_id();
+// Only the small variant should show if anon user.
+if ( ! $user_id && 'small' !== $variant ) {
+	return '';
+}
+
+$is_favorite = is_favorite( $post_id, $user_id );
+$classes = [ 'is-style-text' ];
+if ( $is_favorite ) {
+	$classes[] = 'is-favorite';
+}
+if ( 'small' === $variant ) {
+	$classes[] = 'is-variant-small';
+}
+$classes = implode( ' ',  $classes );
+
+$add_label = __( 'Add to favorites', 'wporg-patterns' );
+$remove_label = __( 'Remove from favorites', 'wporg-patterns' );
+
+$favorite_count = get_favorite_count( $post_id );
+
+$tag_name = ! $user_id ? 'span' : 'button';
+
+?>
+<div
+	<?php echo get_block_wrapper_attributes( [ 'class' => $classes ] ); // phpcs:ignore ?>
+	data-post-id="<?php echo esc_attr( $post_id ); ?>"
+	data-action="<?php echo $is_favorite ? 'remove' : 'add'; ?>"
+>
+	<?php if ( 'small' === $variant ) : ?>
+		<<?php echo $tag_name; ?> class="wporg-favorite-button__button" disabled="disabled">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="pattern-favorite-button__filled" aria-hidden="true" focusable="false">
+  				<path d="M11.941 21.175l-1.443-1.32c-5.124-4.67-8.508-7.75-8.508-11.53 0-3.08 2.408-5.5 5.473-5.5 1.732 0 3.394.81 4.478 2.09 1.085-1.28 2.747-2.09 4.478-2.09 3.065 0 5.473 2.42 5.473 5.5 0 3.78-3.383 6.86-8.508 11.54l-1.443 1.31z"></path>
+			</svg>
+			<span class="wporg-favorite-button__count">
+				<span class="screen-reader-text">
+				<?php
+					printf(
+						_n(
+							'Favorited %s times',
+							'Favorited %s times',
+							$favorite_count,
+							'wporg-patterns'
+						),
+						$favorite_count
+					);
+				?>
+				</span>
+				<span aria-hidden="true"><?php echo $favorite_count; ?></span>
+			</span>
+			<span class="wporg-favorite-button__label screen-reader-text">
+				<?php echo $is_favorite ? $remove_label : $add_label; ?>
+			</span>
+		</<?php echo $tag_name; ?>>
+	<?php else : ?>
+		<button class="wp-block-button__link wp-element-button" disabled="disabled">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="pattern-favorite-button__filled" aria-hidden="true" focusable="false">
+				<path d="M11.941 21.175l-1.443-1.32c-5.124-4.67-8.508-7.75-8.508-11.53 0-3.08 2.408-5.5 5.473-5.5 1.732 0 3.394.81 4.478 2.09 1.085-1.28 2.747-2.09 4.478-2.09 3.065 0 5.473 2.42 5.473 5.5 0 3.78-3.383 6.86-8.508 11.54l-1.443 1.31z"></path>
+			</svg>
+			<span class="wporg-favorite-button__label">
+				<?php echo $is_favorite ? $remove_label : $add_label; ?>
+			</span>
+		</button>
+	<?php endif; ?>
+</div>
