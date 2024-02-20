5f209a80919807e7957709921c491ba1e91a4452
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-20 11:31:15 -0500

    Use mshots for thumbnail preview

--- public_html/wp-content/themes/wporg-pattern-directory-2022/src/blocks/pattern-thumbnail/view.js
+++ public_html/wp-content/themes/wporg-pattern-directory-2022/src/blocks/pattern-thumbnail/view.js
@@ -1,43 +1,88 @@
+/* global FileReader */
 /**
  * WordPress dependencies
  */
-import { getContext, getElement, store } from '@wordpress/interactivity';
+import { getContext, store } from '@wordpress/interactivity';
 
-const { state } = store( 'wporg/patterns/thumbnail', {
+/**
+ * Module constants
+ */
+const MAX_ATTEMPTS = 10;
+const RETRY_DELAY = 2000;
+
+const { actions, state } = store( 'wporg/patterns/thumbnail', {
 	state: {
-		get scale() {
-			const { pageWidth, previewWidth } = getContext();
-			const scale = parseInt( pageWidth, 10 ) / previewWidth;
-			return scale > 1 ? 1 : scale;
+		attempts: 0,
+		shouldRetry: true,
+		hasError: false,
+		get base64Image() {
+			return getContext().base64Image;
 		},
-		get previewHeightCSS() {
-			return `${ getContext().previewHeight }px`;
-		},
-		get iframeWidthCSS() {
-			return `${ getContext().previewWidth }px`;
-		},
-		get iframeHeightCSS() {
-			return `${ getContext().previewHeight / state.scale }px`;
-		},
-		get transformCSS() {
-			return `scale(${ state.scale })`;
+		get hasLoaded() {
+			return state.base64Image || state.hasError;
 		},
 	},
 	actions: {
-		updatePreviewHeight: () => {
-			const context = getContext();
-			const { ref } = getElement();
+		setShouldRetry( value ) {
+			state.shouldRetry = value;
+		},
 
-			const iframeDoc = ref.contentWindow.document;
-			const height = iframeDoc.querySelector( '.entry-content' )?.clientHeight;
-			if ( height ) {
-				context.previewHeight = height * state.scale;
+		setHasError( value ) {
+			state.hasError = value;
+		},
+
+		setBase64Image( value ) {
+			const context = getContext();
+			context.base64Image = value;
+		},
+
+		*fetchImage( fullUrl ) {
+			try {
+				const res = yield fetch( fullUrl );
+				state.attempts++;
+
+				if ( res.redirected ) {
+					actions.setShouldRetry( true );
+				} else if ( res.status === 200 && ! res.redirected ) {
+					const blob = yield res.blob();
+
+					const value = yield new Promise( ( resolve ) => {
+						const reader = new FileReader();
+						reader.onloadend = () => resolve( reader.result );
+						reader.readAsDataURL( blob );
+					} );
+
+					actions.setBase64Image( value );
+					actions.setShouldRetry( false );
+				}
+			} catch ( error ) {
+				actions.setHasError( true );
+				actions.setShouldRetry( false );
 			}
 		},
-		handleOnResize: () => {
-			const context = getContext();
-			const { ref } = getElement();
-			context.pageWidth = ref.clientWidth;
+	},
+
+	callbacks: {
+		// Run on init, starts the image fetch process.
+		*init() {
+			const { src } = getContext();
+
+			if ( ! state.base64Image ) {
+				// Initial fetch.
+				yield actions.fetchImage( src );
+
+				while ( state.shouldRetry ) {
+					yield new Promise( ( resolve ) => {
+						setTimeout( () => resolve(), RETRY_DELAY );
+					} );
+					yield actions.fetchImage( src );
+
+					if ( state.attempts >= MAX_ATTEMPTS ) {
+						actions.setHasError( true );
+						actions.setShouldRetry( false );
+					}
+				}
+			}
 		},
 	},
 } );
