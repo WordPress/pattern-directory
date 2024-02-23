db2dca0a52eff39110bab74cdfab2ce5570688e7
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-23 16:15:28 -0500

    Fix linter issues

--- public_html/wp-content/themes/wporg-pattern-directory-2024/src/blocks/pattern-thumbnail/edit.js
+++ public_html/wp-content/themes/wporg-pattern-directory-2024/src/blocks/pattern-thumbnail/edit.js
@@ -7,7 +7,7 @@ import { useBlockProps } from '@wordpress/block-editor';
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
  *
- * @return {WPElement} Element to render.
+ * @return {Element} Element to render.
  */
 export default function Edit() {
 	return <div { ...useBlockProps() }>Thumbnail</div>;
