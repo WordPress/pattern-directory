68a47c2a8cb3fa6284e34958365d62b212be0fb2
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-27 14:42:13 -0500

    Switch favorite button to interactivity API to simplify label updates

--- public_html/wp-content/themes/wporg-pattern-directory-2024/src/blocks/favorite-button/render.php
+++ public_html/wp-content/themes/wporg-pattern-directory-2024/src/blocks/favorite-button/render.php
@@ -15,60 +15,82 @@ if ( ! $user_id && 'small' !== $variant ) {
 	return '';
 }
 
+// Manually enqueue this script, so that it's available for the interactivity view script.
+wp_enqueue_script( 'wp-api-fetch' );
+
 $is_favorite = is_favorite( $post_id, $user_id );
 $classes = [ 'is-style-text' ];
-if ( $is_favorite ) {
-	$classes[] = 'is-favorite';
-}
 if ( 'small' === $variant ) {
 	$classes[] = 'is-variant-small';
 }
 $classes = implode( ' ',  $classes );
 
-$add_label = __( 'Add to favorites', 'wporg-patterns' );
-$remove_label = __( 'Remove from favorites', 'wporg-patterns' );
-
+$tag_name = ! $user_id ? 'span' : 'button';
 $favorite_count = get_favorite_count( $post_id );
 
-$tag_name = ! $user_id ? 'span' : 'button';
+$add_label = __( 'Add to favorites', 'wporg-patterns' );
+$remove_label = __( 'Remove from favorites', 'wporg-patterns' );
+$sr_label = sprintf(
+	_n(
+		'Favorited %s time',
+		'Favorited %s times',
+		$favorite_count,
+		'wporg-patterns'
+	),
+	$favorite_count
+);
+
+// Initial state to pass to Interactivity API.
+$init_state = [
+	'postId' => $post_id,
+	'isFavorite' => $is_favorite,
+	'count' => $favorite_count,
+	'label' => [
+		'add' => $add_label,
+		'remove' => $remove_label,
+		'screenReader' => __( 'Favorited %s times', 'wporg-patterns' ),
+	]
+];
+$encoded_state = wp_json_encode( $init_state );
 
 ?>
 <div
 	<?php echo get_block_wrapper_attributes( [ 'class' => $classes ] ); // phpcs:ignore ?>
-	data-post-id="<?php echo esc_attr( $post_id ); ?>"
-	data-action="<?php echo $is_favorite ? 'remove' : 'add'; ?>"
+	data-wp-interactive="wporg/patterns/favorite-button"
+	data-wp-context="<?php echo esc_attr( $encoded_state ); ?>"
+	data-wp-class--is-favorite="context.isFavorite"
 >
 	<?php if ( 'small' === $variant ) : ?>
-		<<?php echo $tag_name; ?> class="wporg-favorite-button__button" disabled="disabled">
+		<<?php echo $tag_name; ?>
+			class="wporg-favorite-button__button"
+			disabled="disabled"
+			data-wp-bind--disabled="!context.postId"
+			data-wp-on--click="actions.triggerAction"
+		>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="pattern-favorite-button__filled" aria-hidden="true" focusable="false">
   				<path d="M11.941 21.175l-1.443-1.32c-5.124-4.67-8.508-7.75-8.508-11.53 0-3.08 2.408-5.5 5.473-5.5 1.732 0 3.394.81 4.478 2.09 1.085-1.28 2.747-2.09 4.478-2.09 3.065 0 5.473 2.42 5.473 5.5 0 3.78-3.383 6.86-8.508 11.54l-1.443 1.31z"></path>
 			</svg>
 			<span class="wporg-favorite-button__count">
-				<span class="screen-reader-text">
-				<?php
-					printf(
-						_n(
-							'Favorited %s times',
-							'Favorited %s times',
-							$favorite_count,
-							'wporg-patterns'
-						),
-						$favorite_count
-					);
-				?>
+				<span class="screen-reader-text" data-wp-text="state.labelScreenReader">
+					<?php echo esc_html( $sr_label ); ?>
 				</span>
-				<span aria-hidden="true">(<?php echo $favorite_count; ?>)</span>
+				<span aria-hidden="true" data-wp-text="state.labelCount">(<?php echo $favorite_count; ?>)</span>
 			</span>
-			<span class="wporg-favorite-button__label screen-reader-text">
+			<span class="wporg-favorite-button__label screen-reader-text" data-wp-text="state.labelAction">
 				<?php echo $is_favorite ? $remove_label : $add_label; ?>
 			</span>
 		</<?php echo $tag_name; ?>>
 	<?php else : ?>
-		<button class="wp-block-button__link wp-element-button" disabled="disabled">
+		<button
+			class="wp-block-button__link wp-element-button"
+			disabled="disabled"
+			data-wp-bind--disabled="!context.postId"
+			data-wp-on--click="actions.triggerAction"
+		>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="pattern-favorite-button__filled" aria-hidden="true" focusable="false">
 				<path d="M11.941 21.175l-1.443-1.32c-5.124-4.67-8.508-7.75-8.508-11.53 0-3.08 2.408-5.5 5.473-5.5 1.732 0 3.394.81 4.478 2.09 1.085-1.28 2.747-2.09 4.478-2.09 3.065 0 5.473 2.42 5.473 5.5 0 3.78-3.383 6.86-8.508 11.54l-1.443 1.31z"></path>
 			</svg>
-			<span class="wporg-favorite-button__label">
+			<span class="wporg-favorite-button__label" data-wp-text="state.labelAction">
 				<?php echo $is_favorite ? $remove_label : $add_label; ?>
 			</span>
 		</button>
--- public_html/wp-content/themes/wporg-pattern-directory-2024/src/blocks/favorite-button/view.js
+++ public_html/wp-content/themes/wporg-pattern-directory-2024/src/blocks/favorite-button/view.js
@@ -1,55 +1,47 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import apiFetch from '@wordpress/api-fetch';
+import { getContext, store } from '@wordpress/interactivity';
 
-const init = () => {
-	const containers = document.querySelectorAll( '.wp-block-wporg-favorite-button' );
-	if ( containers ) {
-		containers.forEach( ( element ) => {
-			const button = element.querySelector( 'button' );
-			if ( ! button ) {
-				return;
+store( 'wporg/patterns/favorite-button', {
+	state: {
+		get labelScreenReader() {
+			const { label, count } = getContext();
+			return label.screenReader.replace( '%s', count );
+		},
+		get labelCount() {
+			const { count } = getContext();
+			return `(${ count })`;
+		},
+		get labelAction() {
+			const { label, isFavorite } = getContext();
+			return isFavorite ? label.remove : label.add;
+		},
+	},
+	actions: {
+		*triggerAction() {
+			const context = getContext();
+			if ( context.isFavorite ) {
+				try {
+					const newCount = yield wp.apiFetch( {
+						path: '/wporg/v1/pattern-favorites',
+						method: 'DELETE',
+						data: { id: context.postId },
+					} );
+					context.isFavorite = false;
+					context.count = newCount;
+				} catch ( error ) {}
+			} else {
+				try {
+					const newCount = yield wp.apiFetch( {
+						path: '/wporg/v1/pattern-favorites',
+						method: 'POST',
+						data: { id: context.postId },
+					} );
+					context.isFavorite = true;
+					context.count = newCount;
+				} catch ( error ) {}
 			}
-
-			const label = element.querySelector( '.wp-block-wporg-favorite-button__label' );
-			const countEl = element.querySelector( '.wp-block-wporg-favorite-button__count' );
-			button.disabled = false;
-			button.onclick = async () => {
-				const { action, postId } = element.dataset;
-				if ( action === 'add' ) {
-					try {
-						const newCount = await apiFetch( {
-							path: '/wporg/v1/pattern-favorites',
-							method: 'POST',
-							data: { id: postId },
-						} );
-						element.classList.add( 'is-favorite' );
-						label.innerText = __( 'Remove from favorites', 'wporg-patterns' );
-						element.dataset.action = 'remove';
-						if ( countEl ) {
-							countEl.innerText = newCount;
-						}
-					} catch ( error ) {}
-				} else {
-					try {
-						const newCount = await apiFetch( {
-							path: '/wporg/v1/pattern-favorites',
-							method: 'DELETE',
-							data: { id: postId },
-						} );
-						element.classList.remove( 'is-favorite' );
-						label.innerText = __( 'Add to favorites', 'wporg-patterns' );
-						element.dataset.action = 'add';
-						if ( countEl ) {
-							countEl.innerText = newCount;
-						}
-					} catch ( error ) {}
-				}
-			};
-		} );
-	}
-};
-
-window.addEventListener( 'load', init );
+		},
+	},
+} );
