638ec6971c4eeea23813d767cc1fbbdc98788d23
Kelly Dwan <ryelle@users.noreply.github.com>
2024-03-05 12:14:04 -0500

    Wait for onload event to finish
    On Safari/Chrome, the iframe's readystate can be complete even when it's not, which shorts out this onload resizing.

--- public_html/wp-content/themes/wporg-pattern-directory-2024/src/blocks/pattern-preview/controls/view.js
+++ public_html/wp-content/themes/wporg-pattern-directory-2024/src/blocks/pattern-preview/controls/view.js
@@ -43,14 +43,9 @@ const { actions, state } = store( 'wporg/patterns/preview', {
 		},
 		*onLoad() {
 			const { ref } = getElement();
-			const iframeDoc = ref.contentWindow.document;
 
-			// The iframe might be loaded already, so we attach a listener but also check readyState.
 			yield new Promise( ( resolve ) => {
 				ref.addEventListener( 'load', () => resolve() );
-				if ( 'complete' === iframeDoc.readyState ) {
-					resolve();
-				}
 			} );
 
 			// iframe is loaded now, so we should adjust the height.
@@ -63,7 +58,7 @@ const { actions, state } = store( 'wporg/patterns/preview', {
 			// Need to "use" previewWidth so that `data-wp-watch` will re-run this action when it changes.
 			context.previewWidth; // eslint-disable-line no-unused-expressions
 
-			const iframeDoc = ref.contentWindow.document;
+			const iframeDoc = ref.contentDocument;
 			const height = iframeDoc.querySelector( '.entry-content' )?.clientHeight;
 			if ( height ) {
 				context.previewHeight = height * state.scale;
