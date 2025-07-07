2bb897d6b0e548a130e5cc707167b2cad85c4d2c
Luigi Teschio <gigitux@gmail.com>
2025-07-07 09:49:42 +0200

    Fix setup script

--- bin/index.sh
+++ bin/index.sh
@@ -27,7 +27,7 @@ npm run wp-env run cli wp plugin activate pattern-directory/bootstrap.php
 npm run wp-env run cli wp plugin activate pattern-creator
 
 # Activate theme
-npm run wp-env run cli wp theme activate pattern-directory
+npm run wp-env run cli -- wp theme activate wporg-pattern-directory-2024
 
 # Change permalinks
 npm run wp-env run cli wp rewrite structure '/%postname%/'
@@ -37,8 +37,8 @@ npm run wp-env run cli wp option update blogname "Pattern Directory"
 npm run wp-env run cli wp option update blogdescription "Add a beautifully designed, ready to go layout to any WordPress site with a simple copy/paste."
 
 # Create the default pages
-npm run wp-env run cli wp post create --post_type=page --post_status='publish' --post_name='front-page' --post_title='Pattern Directory'
-npm run wp-env run cli wp post create --post_type=page --post_status='publish' --post_name='archives' --post_title='Archives'
+npm run wp-env run cli -- wp post create --post_type=page --post_status='publish' --post_name='front-page' --post_title='Pattern Directory'
+npm run wp-env run cli -- wp post create --post_type=page --post_status='publish' --post_name='archives' --post_title='Archives'
 
 npm run wp-env run cli wp option update show_on_front 'page'
 npm run wp-env run cli wp option update page_on_front 4
@@ -46,5 +46,5 @@ npm run wp-env run cli wp option update page_for_posts 5
 npm run wp-env run cli wp option update posts_per_page 18
 
 # Import content
-npm run wp-env run cli wp import --authors=create --skip=image_resize wp-content/uploads/data/exports/pattern-dir.000.xml
-npm run wp-env run cli wp import --authors=create --skip=image_resize wp-content/uploads/data/exports/pattern-dir.001.xml
+npm run wp-env run cli -- wp import --authors=create --skip=image_resize wp-content/uploads/data/exports/pattern-dir.000.xml
+npm run wp-env run cli -- wp import --authors=create --skip=image_resize wp-content/uploads/data/exports/pattern-dir.001.xml
