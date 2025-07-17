a3da67f46c2c93bd324dadaa84bcb9c342519034
Luigi Teschio <gigitux@gmail.com>
2025-07-17 04:13:49 +0200

    Fix 'core/interface' registration error (#725)

--- public_html/wp-content/plugins/pattern-creator/webpack.config.js
+++ public_html/wp-content/plugins/pattern-creator/webpack.config.js
@@ -1,4 +1,5 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
 
 const config = {
 	...defaultConfig,
@@ -7,6 +8,25 @@ const config = {
 		library: [ 'wp', 'patternCreator' ],
 		libraryTarget: 'window',
 	},
+
+	plugins: [
+		...defaultConfig.plugins.filter(
+			( plugin ) => plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+		),
+		new DependencyExtractionWebpackPlugin( {
+			requestToExternal( request ) {
+				if (
+					request === '@wordpress/editor' ||
+					request === '@wordpress/icons' ||
+					request === '@wordpress/interface' ||
+					request === '@wordpress/fields' ||
+					request === '@wordpress/dataviews'
+				) {
+					return false;
+				}
+			},
+		} ),
+	],
 };
 
 module.exports = config;
